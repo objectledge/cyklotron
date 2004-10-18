d4ae6a95b6823a23602fc4351b49a964fa069cde
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2004-10-18 06:18:48 +0000

    upgrading CVS tools

--- CVSROOT/logcheck.sh
+++ CVSROOT/logcheck.sh
@@ -9,17 +9,20 @@ cat > $TEMPFILE
 #
 # Invoke spackle
 #
-$CVSROOT/CVSROOT/log_accum.pl $USER "`echo ${1} | cut -d , -f 1`" < $TEMPFILE
+$CVSROOT/CVSROOT/log_accum.pl $USER "${1} ${2}" < $TEMPFILE
+
+REPO=${1}
+shift
 
 #
 # Invoke cvsspam
 #
-$CVSROOT/CVSROOT/collect_diffs.rb --from $USER --to cyklotron-cvshtml@lists.caltha.pl "${1}" < $TEMPFILE
+$CVSROOT/CVSROOT/collect_diffs.rb --from $USER $@ < $TEMPFILE
 
 #
 # Invoke DamageControl
 #
-ruby /home/damagecontrol/current/bin/requestbuild --url http://localhost:4712/private/xmlrpc --projectname `echo ${1} | cut -d/ -f1`
+ruby /home/damagecontrol/current/bin/requestbuild --url http://localhost:4712/private/xmlrpc --projectname `echo $REPO | cut -d / -f 1`
 
 #
 # clean up
