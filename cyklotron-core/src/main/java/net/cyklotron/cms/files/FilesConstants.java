f5b09b5cc3911a1345b8043d10e03c9cf772a65e
Paweł Potempski <pawel.potempski@caltha.pl>
2005-03-10 13:36:41 +0000

    avoid confilct with CMS Constants

--- cyklotron-core/src/main/java/net/cyklotron/cms/files/FilesConstants.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/files/FilesConstants.java
@@ -4,16 +4,16 @@ package net.cyklotron.cms.files;
  * Provide all of the constant definition
  *
  * @author <a href="mailto:pablo@ngo.pl">Pawel Potempski</a>
- * @version $Id: FilesConstants.java,v 1.1 2005-01-12 20:44:42 pablo Exp $
+ * @version $Id: FilesConstants.java,v 1.2 2005-03-10 13:36:41 pablo Exp $
  */
 public interface FilesConstants
 {
     /** state - component configuration/administer */
-    public static final String FROM_COMPONENT = "cms_files_from_component";
+    //public static final String FROM_COMPONENT = "cms_files_from_component";
 
     /** component instance */
-    public static final String COMPONENT_INSTANCE = "cms_files_component_instance";
+    //public static final String COMPONENT_INSTANCE = "cms_files_component_instance";
 
     /** component node */
-    public static final String COMPONENT_NODE = "cms_files_component_node";
+    //public static final String COMPONENT_NODE = "cms_files_component_node";
 }
