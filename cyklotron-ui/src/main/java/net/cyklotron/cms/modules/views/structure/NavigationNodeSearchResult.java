fc6e15f8fbd8ddedd6a211f95da6d0ae29adcb30
Paweł Potempski <pawel.potempski@caltha.pl>
2005-03-10 11:49:07 +0000

    search result redirector

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/structure/NavigationNodeSearchResult.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/structure/NavigationNodeSearchResult.java
@@ -10,6 +10,7 @@ import org.objectledge.table.TableStateManager;
 import org.objectledge.templating.TemplatingContext;
 import org.objectledge.web.HttpContext;
 import org.objectledge.web.mvc.MVCContext;
+import org.objectledge.web.mvc.tools.LinkTool;
 
 import net.cyklotron.cms.CmsDataFactory;
 import net.cyklotron.cms.preferences.PreferencesService;
@@ -17,41 +18,13 @@ import net.cyklotron.cms.related.RelatedService;
 import net.cyklotron.cms.site.SiteService;
 import net.cyklotron.cms.structure.StructureService;
 import net.cyklotron.cms.style.StyleService;
+
 /**
  * Navigation node information screen.
  */
 public class NavigationNodeSearchResult
     extends BaseStructureScreen
 {
-    
-    /**
-     * Builds the screen contents.
-     *
-     * <p>Redirecto to section</p>
-     */
-    //TODO
-    /**
-    public String build(RunData data)
-        throws ProcessingException
-    {
-        LinkTool link = data.getLinkTool();
-        long rid = parameters.getLong("res_id", -1);
-        if(rid == -1)
-        {
-            throw new ProcessingException("Resource id not found");
-        }
-        link = link.set("x",rid).unset("view");
-        try
-        {
-            data.sendRedirect(link.toString());
-        }
-        catch(Exception e)
-        {
-            throw new ProcessingException("Exception occured during redirecting...",e);
-        }
-        return null;
-    }
-    */
     public NavigationNodeSearchResult(Context context, Logger logger,
         PreferencesService preferencesService, CmsDataFactory cmsDataFactory,
         TableStateManager tableStateManager, StructureService structureService,
@@ -62,13 +35,25 @@ public class NavigationNodeSearchResult
         
     }
     
-    
-    /* (non-Javadoc)
-     * @see net.cyklotron.cms.modules.views.BaseCMSScreen#process(org.objectledge.parameters.Parameters, org.objectledge.web.mvc.MVCContext, org.objectledge.templating.TemplatingContext, org.objectledge.web.HttpContext, org.objectledge.i18n.I18nContext, org.objectledge.coral.session.CoralSession)
-     */
     public void process(Parameters parameters, MVCContext mvcContext,
-        TemplatingContext templatingContext, HttpContext httpContext, I18nContext i18nContext,
-        CoralSession coralSession) throws ProcessingException
+        TemplatingContext templatingContext, HttpContext httpContext,
+        I18nContext i18nContext, CoralSession coralSession)
+        throws ProcessingException
     {
+        long rid = parameters.getLong("res_id", -1);
+        if(rid == -1)
+        {
+            throw new ProcessingException("Resource id not found");
+        }
+        LinkTool link = (LinkTool)templatingContext.get("link");
+        link = link.set("x",rid).unsetView();
+        try
+        {
+            httpContext.sendRedirect(link.toString());
+        }
+        catch(Exception e)
+        {
+            throw new ProcessingException("Exception occured during redirecting...",e);
+        }
     }
 }
