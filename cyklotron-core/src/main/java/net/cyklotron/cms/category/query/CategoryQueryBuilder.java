eb0f7e0994775c9fa672842516e0b5c1f7ebdd6a
Damian Gajda <damian.gajda@caltha.pl>
2005-03-29 15:18:09 +0000

    Reimplemented query building to support coral relation queries

--- cyklotron-core/src/main/java/net/cyklotron/cms/category/query/CategoryQueryBuilder.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/category/query/CategoryQueryBuilder.java
@@ -3,20 +3,21 @@ package net.cyklotron.cms.category.query;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
+import net.cyklotron.cms.category.CategoryResource;
+import net.cyklotron.cms.category.internal.CategoryServiceImpl;
+
+import org.objectledge.coral.relation.ResourceHierarchyRelationImpl;
 import org.objectledge.coral.session.CoralSession;
 import org.objectledge.coral.util.ResourceSelectionState;
 import org.objectledge.pipeline.ProcessingException;
 
-import net.cyklotron.cms.category.CategoryResource;
-
 /**
- * A helper class that builds a category query from a ResourceSelectionState.
+ * A helper class that builds a category query string from category sets.
  * 
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: CategoryQueryBuilder.java,v 1.7 2005-02-21 16:29:00 zwierzem Exp $ 
+ * @version $Id: CategoryQueryBuilder.java,v 1.8 2005-03-29 15:18:09 zwierzem Exp $ 
  */
 public class CategoryQueryBuilder
 {
@@ -28,21 +29,41 @@ public class CategoryQueryBuilder
     
     /**
      * Builds a category query from a ResourceSelectionState.
-     * @param resourceService the resource service.
+     * @param coralSession the coral session.
      * @param state categories selection state.
-     * @param idsAsIdentifiers if true category resource ids are used instead of paths to build a query
+     * @param idsAsIdentifiers if true category resource ids are used instead of paths to build
+     *  a query
      * 
      * @throws ProcessingException
      */
-    public CategoryQueryBuilder(CoralSession resourceService, ResourceSelectionState state, 
+    public CategoryQueryBuilder(CoralSession coralSession, ResourceSelectionState state, 
         boolean idsAsIdentifiers)
         throws ProcessingException
     {
-        requiredPart = new QueryPart(resourceService, state, "required", "AND", idsAsIdentifiers);
-        optionalPart = new QueryPart(resourceService, state, "optional", "OR", idsAsIdentifiers);
+        this(
+            (Set<CategoryResource>)(state.getEntities(coralSession, "required").keySet()),
+            (Set<CategoryResource>)(state.getEntities(coralSession, "optional").keySet()),
+            idsAsIdentifiers);
+    }
+
+    /**
+     * Builds a category query from a CategoryResource sets.
+     * @param required the set of required categories.
+     * @param optional the set of optional categories.
+     * @param idsAsIdentifiers if true category resource ids are used instead of paths to build
+     *  a query
+     * 
+     * @throws ProcessingException
+     */
+    public CategoryQueryBuilder(Set<CategoryResource> required, Set<CategoryResource> optional,
+        boolean idsAsIdentifiers)
+    throws ProcessingException
+    {
+        requiredPart = new QueryPart(required, "*", idsAsIdentifiers);
+        optionalPart = new QueryPart(optional, "+", idsAsIdentifiers);
         if(requiredPart.length() > 0 && optionalPart.length() > 0)
         {
-            query = requiredPart.getPart() + " AND " + optionalPart.getPart() + ";";
+            query = requiredPart.getPart() + " * " + optionalPart.getPart() + ";";
         }
         else if(requiredPart.length() > 0)
         {
@@ -53,7 +74,7 @@ public class CategoryQueryBuilder
             query = optionalPart.getPart() + ";";
         }
     }
-
+    
     /**
      * Returns query body.
      * 
@@ -89,23 +110,16 @@ public class CategoryQueryBuilder
         public String[] identifiers;
         public String queryPart;
 
-        public QueryPart(
-        	CoralSession resourceService,
-        	ResourceSelectionState state,
-        	String selectionState,
-        	String queryOperator, boolean idsAsIdentifiers)
+        public QueryPart(Set<CategoryResource> categories, String queryOperator, boolean idsAsIdentifiers)
             throws ProcessingException
         {
-            Map temp = state.getEntities(resourceService, selectionState);
-            Set categories = temp.keySet();
-
             StringBuilder queryPartBuffer = new StringBuilder();
             List identifiersList = new ArrayList(categories.size());
 
             int j = 0;
-            for (Iterator i = categories.iterator(); i.hasNext(); j++)
+            for (Iterator<CategoryResource> i = categories.iterator(); i.hasNext(); j++)
             {
-                CategoryResource cat = (CategoryResource) (i.next());
+                CategoryResource cat = i.next();
                 String id = cat.getIdString();
 
                 if (j == 0)
@@ -121,7 +135,15 @@ public class CategoryQueryBuilder
 
                 String categoryIdentifier = 
                     (idsAsIdentifiers) ? getCategoryId(cat) : getCategoryPath(cat);
-                queryPartBuffer.append(categoryIdentifier);
+
+                // MAP('category.References'){ MAPTRANS('resource.Hierarchy'){ RES(1234) } }
+                queryPartBuffer.append("MAP('")
+                    .append(CategoryServiceImpl.CATEGORY_RESOURCE_RELATION_NAME)
+                    .append("'){ MAPTRANS('")
+                    .append(ResourceHierarchyRelationImpl.NAME)
+                    .append("'){ RES(")
+                    .append(categoryIdentifier)
+                    .append(") } }");
                 identifiersList.add(categoryIdentifier);
                 
                 if (!i.hasNext())
