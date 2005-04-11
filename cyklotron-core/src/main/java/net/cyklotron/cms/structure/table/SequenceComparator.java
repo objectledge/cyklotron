726024aec07fc0955d746c77a2b35c443795b905
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2005-04-11 10:34:23 +0000

    maintain stable ordering

--- cyklotron-core/src/main/java/net/cyklotron/cms/structure/table/SequenceComparator.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/structure/table/SequenceComparator.java
@@ -8,7 +8,7 @@ import net.cyklotron.cms.structure.NavigationNodeResource;
  * A copmparator used for sorting navigation nodes according to their sequence
  *
  * @author <a href="mailto:pablo@ngo.pl">Pawel Potempski</a>
- * @version $Id: SequenceComparator.java,v 1.2 2005-01-12 20:57:15 pablo Exp $
+ * @version $Id: SequenceComparator.java,v 1.3 2005-04-11 10:34:23 rafal Exp $
  */
 public class SequenceComparator implements Comparator
 {
@@ -20,6 +20,12 @@ public class SequenceComparator implements Comparator
         }
         NavigationNodeResource n1 = (NavigationNodeResource) o1;
         NavigationNodeResource n2 = (NavigationNodeResource) o2;
-        return n1.getSequence(-1) - n2.getSequence(-1);
+        int r = n1.getSequence(-1) - n2.getSequence(-1);
+        // same sequence value? - strive for stable ordering anyway
+        if(r == 0)
+        {
+            r = (int)(n1.getId() - n2.getId());
+        }
+        return r;
     }
 }
