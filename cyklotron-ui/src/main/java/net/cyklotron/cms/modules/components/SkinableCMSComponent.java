243896e056b631c85eef061c2a12bec4e1a0319f
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2005-04-21 07:41:45 +0000

    fix ,->. problem when no template is provied by the skin

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/components/SkinableCMSComponent.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/components/SkinableCMSComponent.java
@@ -22,7 +22,7 @@ import net.cyklotron.cms.skins.SkinService;
  * The base class for skinable CMS components
  *
  * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
- * @version $Id: SkinableCMSComponent.java,v 1.7 2005-03-24 05:53:30 pablo Exp $
+ * @version $Id: SkinableCMSComponent.java,v 1.8 2005-04-21 07:41:45 rafal Exp $
  */
 public abstract class SkinableCMSComponent
     extends BaseCMSComponent
@@ -125,7 +125,7 @@ public abstract class SkinableCMSComponent
         {
             component = component + state;
         }
-        Template template = finderService.getComponentTemplate(component);
+        Template template = finderService.getComponentTemplate(component.replace(",","."));
         return template;
     }
 
