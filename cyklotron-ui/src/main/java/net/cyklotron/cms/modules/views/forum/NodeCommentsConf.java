b7b93982a74ef7f07ae45bf1e058116163f68da4
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2005-06-02 09:42:05 +0000

    CYKLO-259 added configurator for the NodeComments component.

---
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/forum/NodeCommentsConf.java
@@ -0,0 +1,59 @@
+package net.cyklotron.cms.modules.views.forum;
+
+import org.jcontainer.dna.Logger;
+import org.objectledge.context.Context;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.i18n.I18nContext;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.table.TableStateManager;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.HttpContext;
+import org.objectledge.web.mvc.MVCContext;
+
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.i18n.I18nContext;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.HttpContext;
+import org.objectledge.web.mvc.MVCContext;
+
+import net.cyklotron.cms.CmsDataFactory;
+import net.cyklotron.cms.modules.views.BaseCMSScreen;
+import net.cyklotron.cms.preferences.PreferencesService;
+
+/**
+ * Configurator screen for NodeComments component.
+ */
+public class NodeCommentsConf
+    extends BaseCMSScreen
+{
+    /**
+     * Craetes a new CodeComments component configuration view insntace.
+     * 
+     * @param context the Context component.
+     * @param logger the Logger.
+     * @param preferencesService the PreferencesService component.
+     * @param cmsDataFactory the CmsDataFactory component.
+     * @param tableStateManager the TableStateManager component.
+     */
+    public NodeCommentsConf(Context context, Logger logger, PreferencesService preferencesService,
+        CmsDataFactory cmsDataFactory, TableStateManager tableStateManager)
+    {
+        super(context, logger, preferencesService, cmsDataFactory, tableStateManager);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void process(Parameters parameters, MVCContext mvcContext,
+        TemplatingContext templatingContext, HttpContext httpContext, I18nContext i18nContext,
+        CoralSession coralSession)
+        throws ProcessingException
+    {
+        templatingContext.put("componentConfig", prepareComponentConfig(parameters,
+            templatingContext));
+    }
+}
