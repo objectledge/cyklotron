79ea60122a9eb0c2fa8a018675061e114ad1c9bb
Damian Gajda <damian.gajda@caltha.pl>
2005-06-16 13:51:12 +0000

    Adapted the Listener to new interfaces

--- cyklotron-core/src/main/java/net/cyklotron/cms/syndication/SyndicationListener.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/syndication/SyndicationListener.java
@@ -1,33 +1,41 @@
 package net.cyklotron.cms.syndication;
 
-import org.jcontainer.dna.Logger;
-import org.objectledge.coral.security.Role;
-import org.objectledge.coral.session.CoralSession;
-import org.objectledge.coral.session.CoralSessionFactory;
-import org.objectledge.event.EventWhiteboard;
-
 import net.cyklotron.cms.security.CmsSecurityException;
 import net.cyklotron.cms.security.SecurityService;
 import net.cyklotron.cms.site.BaseSiteListener;
 import net.cyklotron.cms.site.SiteCreationListener;
+import net.cyklotron.cms.site.SiteDestructionValve;
 import net.cyklotron.cms.site.SiteException;
 import net.cyklotron.cms.site.SiteResource;
 import net.cyklotron.cms.site.SiteService;
+
+import org.jcontainer.dna.Logger;
+import org.objectledge.coral.security.Role;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.session.CoralSessionFactory;
+import org.objectledge.coral.store.Resource;
+import org.objectledge.event.EventWhiteboard;
+import org.picocontainer.Startable;
 
 /**
  * Syndication Site Creation Listener implementation
  *
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: SyndicationListener.java,v 1.1 2005-06-16 11:14:22 zwierzem Exp $
+ * @version $Id: SyndicationListener.java,v 1.2 2005-06-16 13:51:12 zwierzem Exp $
  */
 public class SyndicationListener 
     extends BaseSiteListener
-    implements SiteCreationListener
+    implements SiteCreationListener, SiteDestructionValve, Startable
 {
+    
+    private SyndicationService syndicationService;
+    
     public SyndicationListener(Logger logger, CoralSessionFactory sessionFactory,
-        SecurityService cmsSecurityService, EventWhiteboard eventWhiteboard)
+        SecurityService cmsSecurityService, EventWhiteboard eventWhiteboard,
+        SyndicationService syndicationService)
     {
         super(logger, sessionFactory, cmsSecurityService, eventWhiteboard);
+        this.syndicationService = syndicationService;
     }
 
     // listeners implementation ////////////////////////////////////////////////////////
@@ -68,5 +76,31 @@ public class SyndicationListener
             "cms.syndication.infeed.administrator", site);
         cmsSecurityService.createRole(coralSession, administrator, 
             "cms.syndication.outfeed.administrator", site);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void clearApplication(CoralSession coralSession, SiteService siteService, SiteResource site) throws Exception
+    {
+        Resource root = syndicationService.getAppParent(coralSession, site);
+        if(root == null)
+        {
+            return;
+        }
+        deleteSiteNode(coralSession, root);
+    }
+    
+    public void clearSecurity(CoralSession coralSession, SiteService siteService, SiteResource site) throws Exception
+    {
+        
+    }
+
+    public void start()
+    {
+    }
+
+    public void stop()
+    {
     }
 }
