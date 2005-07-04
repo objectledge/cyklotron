77aa22bf83ad22812c786a4e02f48d771ed40447
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2005-07-04 09:52:45 +0000

    added results caching capability to CategoryQueryResults screen

--- cyklotron-core/src/main/java/net/cyklotron/cms/category/query/CategoryQueryResultsConfiguration.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/category/query/CategoryQueryResultsConfiguration.java
@@ -8,7 +8,7 @@ import net.cyklotron.cms.category.components.DocumentResourceListConfiguration;
  * Provides default parameter values for category query results screen configuration.
  * 
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: CategoryQueryResultsConfiguration.java,v 1.3 2005-02-09 22:21:19 rafal Exp $ 
+ * @version $Id: CategoryQueryResultsConfiguration.java,v 1.4 2005-07-04 09:52:39 rafal Exp $ 
  */
 public class CategoryQueryResultsConfiguration
 extends DocumentResourceListConfiguration
@@ -25,5 +25,11 @@ extends DocumentResourceListConfiguration
             this.categoryQueryName = categoryQuery.getName();
         }
         shortInit(screenConfig);
+        this.categoryQuery = categoryQuery;
 	}
+    
+    public CategoryQueryResource getCategoryQuery()
+    {
+        return categoryQuery;
+    }
 }
--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/category/query/CategoryQueryResults.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/category/query/CategoryQueryResults.java
@@ -1,22 +1,12 @@
 package net.cyklotron.cms.modules.views.category.query;
 
+import java.util.Map;
 import java.util.Set;
-
-import org.jcontainer.dna.Logger;
-import org.objectledge.context.Context;
-import org.objectledge.coral.session.CoralSession;
-import org.objectledge.coral.store.Resource;
-import org.objectledge.parameters.Parameters;
-import org.objectledge.parameters.RequestParameters;
-import org.objectledge.pipeline.ProcessingException;
-import org.objectledge.table.TableState;
-import org.objectledge.table.TableStateManager;
-import org.objectledge.table.TableTool;
-import org.objectledge.templating.TemplatingContext;
-import org.objectledge.web.mvc.finders.MVCFinder;
 
 import net.cyklotron.cms.CmsData;
 import net.cyklotron.cms.CmsDataFactory;
+import net.cyklotron.cms.category.components.BaseResourceList;
+import net.cyklotron.cms.category.components.BaseResourceListConfiguration;
 import net.cyklotron.cms.category.query.CategoryQueryException;
 import net.cyklotron.cms.category.query.CategoryQueryResource;
 import net.cyklotron.cms.category.query.CategoryQueryResultsConfiguration;
@@ -31,11 +21,25 @@ import net.cyklotron.cms.skins.SkinService;
 import net.cyklotron.cms.structure.StructureService;
 import net.cyklotron.cms.style.StyleService;
 
+import org.jcontainer.dna.Logger;
+import org.objectledge.cache.CacheFactory;
+import org.objectledge.context.Context;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.store.Resource;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.parameters.RequestParameters;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.table.TableState;
+import org.objectledge.table.TableStateManager;
+import org.objectledge.table.TableTool;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.mvc.finders.MVCFinder;
+
 /**
  * Category Query Resutls screen.
  * 
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: CategoryQueryResults.java,v 1.4 2005-03-23 09:14:07 pablo Exp $ 
+ * @version $Id: CategoryQueryResults.java,v 1.5 2005-07-04 09:52:41 rafal Exp $ 
  */
 public class CategoryQueryResults 
     extends BaseSkinableScreen
@@ -47,18 +51,21 @@ public class CategoryQueryResults
     
     protected IntegrationService integrationService;
     
+    protected CacheFactory cacheFactory;
+    
     public CategoryQueryResults(org.objectledge.context.Context context, Logger logger,
         PreferencesService preferencesService, CmsDataFactory cmsDataFactory,
         StructureService structureService, StyleService styleService, SkinService skinService,
         MVCFinder mvcFinder, TableStateManager tableStateManager,
         CategoryQueryService categoryQueryService, SiteService siteService,
-        IntegrationService integrationService)
+        IntegrationService integrationService, CacheFactory cacheFactory)
     {
         super(context, logger, preferencesService, cmsDataFactory, structureService, styleService,
                         skinService, mvcFinder, tableStateManager);
         this.categoryQueryService = categoryQueryService;
         this.siteService = siteService;
         this.integrationService = integrationService;
+        this.cacheFactory = cacheFactory;
 	}
 	
 	public void prepareDefault(Context context)
@@ -105,18 +112,9 @@ public class CategoryQueryResults
 
         // get resources based on category query
         Resource[] resources = null;
-        String query = resList.getQuery(coralSession, config);
-        Set idSet = resList.getIdSet(coralSession, config);
         try
         {
-            if(idSet != null)
-            {
-                resources = categoryQueryService.forwardQuery(coralSession, query, idSet);
-            }
-            else
-            {
-                resources = categoryQueryService.forwardQuery(coralSession, query);
-            }
+            resources = getResources(coralSession, resList, config);
         }
         catch(Exception e)
         {
@@ -129,4 +127,91 @@ public class CategoryQueryResults
         TableTool tool = resList.getTableTool(coralSession, context, config, state, resources);
         templatingContext.put("table", tool);
     }
+    
+    private Resource[] getResources(CoralSession coralSession, BaseResourceList resList,
+        CategoryQueryResultsConfiguration config)
+        throws Exception
+    {
+        long cacheInterval = (long) config.getCacheInterval();
+        if(cacheInterval > 0L)
+        {
+            // get cache instance
+            Map cache = null;
+            Map keyCache = null;
+            try
+            {
+                cache = cacheFactory.getInstance("categoryquery", "categoryquery");
+                keyCache = cacheFactory.getInstance("categoryquerykey", "categoryquerykey");
+            }
+            catch(Exception e)
+            {
+                throw new ProcessingException(e);
+            }
+            CmsData cmsData = cmsDataFactory.getCmsData(context); 
+            String key = cmsData.getNode().getIdString() + "."
+                + config.getCategoryQuery().getIdString();
+            String synchronizedKey = null;
+            synchronized(keyCache)
+            {
+                synchronizedKey = (String)keyCache.get(key);
+                if(synchronizedKey == null)
+                {
+                    synchronizedKey = key;
+                    keyCache.put(key, key);
+                }
+            }
+            synchronized(synchronizedKey)
+            {
+                // get cached resource list together with creation time
+                CacheEntry entry = (CacheEntry) cache.get(key);
+                // check entry validity
+                if(entry == null ||
+                System.currentTimeMillis() - entry.timeStamp > cacheInterval*1000L)
+                {
+                    Resource[] ress = getResources2(coralSession, resList, config);
+                    entry = new CacheEntry(ress, System.currentTimeMillis());
+                    synchronized (cache)
+                    {
+                        cache.put(key, entry);
+                    }
+                }
+                return entry.list;
+            }            
+        }
+        else
+        {
+            CmsData cmsData = cmsDataFactory.getCmsData(context);
+            logger.warn("non-cachable category query results screen nodeId="
+                + cmsData.getNode().getIdString());
+            return getResources2(coralSession, resList, config);
+        }
+    }
+    
+    private Resource[] getResources2(CoralSession coralSession, BaseResourceList resList,
+        BaseResourceListConfiguration config)
+        throws Exception
+    {
+        String query = resList.getQuery(coralSession, config);
+        Set idSet = resList.getIdSet(coralSession, config);
+        if(idSet != null)
+        {
+            return categoryQueryService.forwardQuery(coralSession, query, idSet);
+        }
+        else
+        {
+            return categoryQueryService.forwardQuery(coralSession, query);
+        }        
+    }
+    
+    private class CacheEntry
+    {
+        final private Resource[] list;
+        final private long timeStamp;
+
+        public CacheEntry(Resource[] list, long timeStamp)
+        {
+            this.list = list;
+            this.timeStamp = timeStamp;
+        }
+    }    
 }
