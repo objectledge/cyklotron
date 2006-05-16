710a0621a5d1f21a8d4e5eb18af1d0d09088ed8a
Paweł Potempski <pawel.potempski@caltha.pl>
2006-05-16 09:58:19 +0000

    allow to delete footer

---
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/documents/DeleteFooter.java
@@ -0,0 +1,51 @@
+package net.cyklotron.cms.modules.actions.documents;
+
+import org.jcontainer.dna.Logger;
+import org.objectledge.context.Context;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.store.Resource;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.HttpContext;
+import org.objectledge.web.mvc.MVCContext;
+
+import pl.caltha.forms.FormsService;
+
+import net.cyklotron.cms.CmsDataFactory;
+import net.cyklotron.cms.documents.DocumentService;
+import net.cyklotron.cms.documents.FooterResource;
+import net.cyklotron.cms.documents.FooterResourceImpl;
+import net.cyklotron.cms.site.SiteResource;
+import net.cyklotron.cms.structure.StructureService;
+import net.cyklotron.cms.style.StyleService;
+
+/**
+ *
+ * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
+ * @version $Id: DeleteFooter.java,v 1.1 2006-05-16 09:58:19 pablo Exp $
+ */
+public class DeleteFooter extends BaseDocumentAction
+{
+    public DeleteFooter(Logger logger, StructureService structureService,
+        CmsDataFactory cmsDataFactory, StyleService styleService, FormsService formsService,
+        DocumentService documentService)
+    {
+        super(logger, structureService, cmsDataFactory, styleService, formsService, documentService);
+    }
+    
+    public void execute(Context context, Parameters parameters, MVCContext mvcContext, TemplatingContext templatingContext, HttpContext httpContext, CoralSession coralSession)
+        throws ProcessingException
+    {
+        try
+        {
+            long footerId = parameters.getLong("footerId");
+            FooterResource footer = FooterResourceImpl.getFooterResource(coralSession, footerId);
+            coralSession.getStore().deleteResource(footer);
+        }
+        catch(Exception e)
+        {
+            throw new ProcessingException("failed to add footer", e);
+        }
+    }
+}
