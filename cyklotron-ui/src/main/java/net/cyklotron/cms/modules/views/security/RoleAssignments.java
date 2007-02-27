48024fbd2ac2f49a3f171841e9875be3c95c09ce
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-02-27 22:13:21 +0000

    CYKLO-446 improvements in subject list presentation: paging, sorting & filtering

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/security/RoleAssignments.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/security/RoleAssignments.java
@@ -1,16 +1,25 @@
 package net.cyklotron.cms.modules.views.security;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.jcontainer.dna.Logger;
+import org.objectledge.authentication.UserManager;
 import org.objectledge.coral.security.Role;
 import org.objectledge.coral.security.Subject;
 import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.table.SubjectListTableModel;
+import org.objectledge.coral.table.filter.PersonalDataFilter;
 import org.objectledge.i18n.I18nContext;
 import org.objectledge.parameters.Parameters;
 import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.table.TableFilter;
+import org.objectledge.table.TableModel;
+import org.objectledge.table.TableState;
 import org.objectledge.table.TableStateManager;
+import org.objectledge.table.TableTool;
 import org.objectledge.templating.TemplatingContext;
 import org.objectledge.web.HttpContext;
 import org.objectledge.web.mvc.MVCContext;
@@ -25,14 +34,16 @@ import net.cyklotron.cms.site.SiteResource;
 public class RoleAssignments
     extends BaseRoleScreen
 {
+    private final UserManager userManager;
 
-    
     public RoleAssignments(org.objectledge.context.Context context, Logger logger,
         PreferencesService preferencesService, CmsDataFactory cmsDataFactory,
-        TableStateManager tableStateManager, SecurityService securityService)
+        TableStateManager tableStateManager, SecurityService securityService,
+        UserManager userManager)
     {
         super(context, logger, preferencesService, cmsDataFactory, tableStateManager,
                         securityService);
+        this.userManager = userManager;
         
     }
     public void process(Parameters parameters, MVCContext mvcContext, TemplatingContext templatingContext, HttpContext httpContext, I18nContext i18nContext, CoralSession coralSession)
@@ -41,14 +52,38 @@ public class RoleAssignments
         try
         {
             SiteResource site = getSite();
+            Subject[] subjects;
             if(site != null)
             {
-                templatingContext.put("subjects", site.getTeamMember().getSubjects());
+                subjects = site.getTeamMember().getSubjects();
             }
             else
             {
-                templatingContext.put("subjects", coralSession.getSecurity().getSubject());
+                subjects = coralSession.getSecurity().getSubject();
             }
+            String properties[] = { "uid", "sn", "givenName", "mail" };
+            TableModel model = new SubjectListTableModel(subjects, i18nContext.getLocale(),
+                properties, userManager);
+            String suffix = "";
+            if(site != null)
+            {
+                suffix = site.getName();
+            }
+            TableState state = tableStateManager.getState(context, "cms.security.RoleAssignments:"+suffix);
+            if(state.isNew())
+            {
+                state.setTreeView(false);
+                state.setPageSize(20);
+            }
+            List<TableFilter<Subject>> filters = new ArrayList<TableFilter<Subject>>();
+            String filterPattern = parameters.get("filter", "");
+            if(filterPattern.length() > 0)
+            {
+                filters.add(new PersonalDataFilter(properties, parameters.get("filter"),
+                    userManager));
+                templatingContext.put("filter", filterPattern);
+            }
+            templatingContext.put("table", new TableTool(state, filters, model));
             long roleId = parameters.getLong("role_id");
             RoleResource role = RoleResourceImpl.getRoleResource(coralSession, roleId);
             templatingContext.put("role", role);
