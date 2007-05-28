add7ff827190fbb74966ed34b1924e99a1b1f539
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-05-28 20:31:56 +0000

    CYKLO-445 use rootSession to manipulate permissions

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/security/CreateRole.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/security/CreateRole.java
@@ -1,40 +1,45 @@
 package net.cyklotron.cms.modules.actions.security;
 
+import net.cyklotron.cms.CmsDataFactory;
+import net.cyklotron.cms.security.SecurityService;
+import net.cyklotron.cms.site.SiteResource;
+import net.cyklotron.cms.structure.StructureService;
+
 import org.jcontainer.dna.Logger;
 import org.objectledge.authentication.UserManager;
 import org.objectledge.context.Context;
 import org.objectledge.coral.security.Role;
 import org.objectledge.coral.security.Subject;
 import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.session.CoralSessionFactory;
 import org.objectledge.coral.store.Resource;
 import org.objectledge.parameters.Parameters;
 import org.objectledge.pipeline.ProcessingException;
 import org.objectledge.templating.TemplatingContext;
+import org.objectledge.utils.StackTrace;
 import org.objectledge.web.HttpContext;
 import org.objectledge.web.mvc.MVCContext;
-
-import net.cyklotron.cms.CmsDataFactory;
-import net.cyklotron.cms.security.SecurityService;
-import net.cyklotron.cms.site.SiteResource;
-import net.cyklotron.cms.structure.StructureService;
 
 public class CreateRole
     extends BaseSecurityAction
 {
+    private CoralSessionFactory coralSessionFactory;
+    
     public CreateRole(Logger logger, StructureService structureService,
-        CmsDataFactory cmsDataFactory, SecurityService cmsSecurityService, UserManager userManager)
+        CmsDataFactory cmsDataFactory, SecurityService cmsSecurityService, UserManager userManager,
+        CoralSessionFactory coralSessionFactory)
     {
         super(logger, structureService, cmsDataFactory, cmsSecurityService, userManager);
-        
+        this.coralSessionFactory = coralSessionFactory;    
     }
     
     
     public void execute(Context context, Parameters parameters, MVCContext mvcContext, TemplatingContext templatingContext, HttpContext httpContext, CoralSession coralSession)
         throws ProcessingException
     {
+        CoralSession rootSession = coralSessionFactory.getRootSession();
         try
         {
-            Subject subject = coralSession.getUserSubject();
             String roleName = parameters.get("role","");
             if(roleName.length() == 0)
             {
@@ -45,20 +50,20 @@ public class CreateRole
             {
                 throw new ProcessingException("Couldn't find resource_id parameter");
             }
-            Resource resource = coralSession.getStore().getResource(resourceId);
+            Resource resource = rootSession.getStore().getResource(resourceId);
             
             long parentRoleId = parameters.getLong("parent_id", -1);
             Role parentRole = null;
             if(parentRoleId != -1)
             {
-                parentRole = coralSession.getSecurity().getRole(parentRoleId);
+                parentRole = rootSession.getSecurity().getRole(parentRoleId);
             }
             else
             {
                 SiteResource site = getSite(context);
                 if(site == null)
                 {
-                    parentRole = coralSession.getSecurity().
+                    parentRole = rootSession.getSecurity().
                         getUniqueRole("cms.administrator");
                 }
                 else
@@ -66,11 +71,17 @@ public class CreateRole
                     parentRole = site.getAdministrator();
                 }
             }
-            cmsSecurityService.createRole(coralSession, parentRole, roleName, resource);
+            cmsSecurityService.createRole(rootSession, parentRole, roleName, resource);
         }
         catch(Exception e)
         {
-            throw new ProcessingException("failed to create role", e);
+            templatingContext.put("result", "exception");
+            templatingContext.put("trace", new StackTrace(e));
+
+        }
+        finally
+        {
+            rootSession.close();
         }
     }
 }
