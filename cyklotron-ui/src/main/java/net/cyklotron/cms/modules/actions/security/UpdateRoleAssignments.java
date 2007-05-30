5d0ef1c8135d4deb9c3229e297f8459b56905cc4
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-05-30 14:49:45 +0000

    CYKLO-445 use rootSession to manipulate permissions

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/security/UpdateRoleAssignments.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/security/UpdateRoleAssignments.java
@@ -1,7 +1,9 @@
 package net.cyklotron.cms.modules.actions.security;
 
-import java.util.HashSet;
-import java.util.Iterator;
+import net.cyklotron.cms.CmsDataFactory;
+import net.cyklotron.cms.security.RoleResource;
+import net.cyklotron.cms.security.SecurityService;
+import net.cyklotron.cms.structure.StructureService;
 
 import org.jcontainer.dna.Logger;
 import org.objectledge.authentication.UserManager;
@@ -10,6 +12,7 @@ import org.objectledge.coral.security.Role;
 import org.objectledge.coral.security.RoleAssignment;
 import org.objectledge.coral.security.Subject;
 import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.session.CoralSessionFactory;
 import org.objectledge.parameters.Parameters;
 import org.objectledge.pipeline.ProcessingException;
 import org.objectledge.templating.TemplatingContext;
@@ -17,47 +20,49 @@ import org.objectledge.utils.StackTrace;
 import org.objectledge.web.HttpContext;
 import org.objectledge.web.mvc.MVCContext;
 
-import net.cyklotron.cms.CmsDataFactory;
-import net.cyklotron.cms.security.RoleResource;
-import net.cyklotron.cms.security.SecurityService;
-import net.cyklotron.cms.structure.StructureService;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class UpdateRoleAssignments
     extends BaseSecurityAction
 {
+    private CoralSessionFactory coralSessionFactory;
+    
     public UpdateRoleAssignments(Logger logger, StructureService structureService,
-        CmsDataFactory cmsDataFactory, SecurityService cmsSecurityService, UserManager userManager)
+        CmsDataFactory cmsDataFactory, SecurityService cmsSecurityService, UserManager userManager,
+        CoralSessionFactory coralSessionFactory)
     {
         super(logger, structureService, cmsDataFactory, cmsSecurityService, userManager);
-        
+        this.coralSessionFactory = coralSessionFactory;
     }
+    
     public void execute(Context context, Parameters parameters, MVCContext mvcContext, TemplatingContext templatingContext, HttpContext httpContext, CoralSession coralSession)
         throws ProcessingException
     {
+        CoralSession rootSession = coralSessionFactory.getRootSession();
         try
         {
             long roleId = parameters.getLong("role_id");
-            RoleResource roleRes = (RoleResource)coralSession.getStore().getResource(roleId);
+            RoleResource roleRes = (RoleResource)rootSession.getStore().getResource(roleId);
             Role role = roleRes.getRole();
             long[] subjectIds = parameters.getLongs("subject_id");
-            HashSet subjects = new HashSet();
+            HashSet<Subject> subjects = new HashSet<Subject>();
             for(int i=0; i<subjectIds.length; i++)
             {
-                subjects.add(coralSession.getSecurity().getSubject(subjectIds[i]));
+                subjects.add(rootSession.getSecurity().getSubject(subjectIds[i]));
             }
             long[] selectedSubjectIds = parameters.getLongs("selected_subject_id");
-            HashSet selectedSubjects = new HashSet();
+            HashSet<Subject> selectedSubjects = new HashSet<Subject>();
             for(int i=0; i<selectedSubjectIds.length; i++)
             {
-                selectedSubjects.add(coralSession.getSecurity().getSubject(selectedSubjectIds[i]));
+                selectedSubjects.add(rootSession.getSecurity().getSubject(selectedSubjectIds[i]));
             }
             RoleAssignment[] roleAssignments = role.getRoleAssignments();
-            HashSet roleSubjects = new HashSet();
+            HashSet<Subject> roleSubjects = new HashSet<Subject>();
             for(int i=0; i<roleAssignments.length; i++)
             {
                 roleSubjects.add(roleAssignments[i].getSubject());
             }
-            Subject root = coralSession.getSecurity().getSubject(Subject.ROOT);
             Iterator i = subjects.iterator();
             while(i.hasNext())
             {
@@ -66,14 +71,14 @@ public class UpdateRoleAssignments
                 {
                     if(!roleSubjects.contains(subject))
                     {
-                        coralSession.getSecurity().grant(role, subject, false);
+                        rootSession.getSecurity().grant(role, subject, false);
                     }
                 }
                 else
                 {
                     if(roleSubjects.contains(subject))
                     {
-                        coralSession.getSecurity().revoke(role, subject);
+                        rootSession.getSecurity().revoke(role, subject);
                     }
                 }
             }
@@ -81,9 +86,12 @@ public class UpdateRoleAssignments
         }
         catch(Exception e)
         {
-            // log.error("AddMember", e);
             templatingContext.put("result", "exception");
             templatingContext.put("trace", new StackTrace(e));
+        }
+        finally
+        {
+            rootSession.close();
         }
     }
 }
