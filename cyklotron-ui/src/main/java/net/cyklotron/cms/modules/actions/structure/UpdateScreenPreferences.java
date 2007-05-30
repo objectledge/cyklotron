fd5d90da842bc92e83b808615c52d8601baddf27
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-05-30 20:14:30 +0000

    CYKLO-453 get rid of non intentional dependency on obscure internal JDK class

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/structure/UpdateScreenPreferences.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/structure/UpdateScreenPreferences.java
@@ -1,12 +1,5 @@
 package net.cyklotron.cms.modules.actions.structure;
 
-import org.jcontainer.dna.Logger;
-import org.objectledge.context.Context;
-import org.objectledge.coral.session.CoralSession;
-import org.objectledge.parameters.DefaultParameters;
-import org.objectledge.parameters.Parameters;
-import org.objectledge.pipeline.ProcessingException;
-
 import net.cyklotron.cms.CmsDataFactory;
 import net.cyklotron.cms.preferences.PreferencesService;
 import net.cyklotron.cms.site.SiteService;
@@ -14,7 +7,12 @@ import net.cyklotron.cms.structure.NavigationNodeResource;
 import net.cyklotron.cms.structure.StructureService;
 import net.cyklotron.cms.style.StyleService;
 
-import com.sun.org.apache.bcel.internal.verifier.exc.LoadingException;
+import org.jcontainer.dna.Logger;
+import org.objectledge.context.Context;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.parameters.DefaultParameters;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.pipeline.ProcessingException;
 
 public class UpdateScreenPreferences extends BaseUpdatePreferences
 {
@@ -47,7 +45,7 @@ public class UpdateScreenPreferences extends BaseUpdatePreferences
         {
             conf.add(new DefaultParameters(config), true);
         }
-        catch(LoadingException e)
+        catch(Exception e)
         {
             throw new ProcessingException("Failed to parse provided configuration string", e);
         }
