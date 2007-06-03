ca4dbb69cec79df56bc30dafa2ed64469e1abaea
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-06-03 00:14:22 +0000

    merging HEAD changes to 2.3 branch

--- cyklotron-core/src/main/java/net/cyklotron/cms/integration/internal/IntegrationServiceImpl.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/integration/internal/IntegrationServiceImpl.java
@@ -36,7 +36,7 @@ import net.cyklotron.cms.site.SiteResource;
  * @author <a href="mailto:rkrzewsk@caltha.pl">Rafal Krzewski</a>
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
  * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
- * @version $Id: IntegrationServiceImpl.java,v 1.16 2007-02-25 13:57:04 rafal Exp $
+ * @version $Id: IntegrationServiceImpl.java,v 1.16.2.1 2007-06-03 00:14:22 rafal Exp $
  */
 public class IntegrationServiceImpl
     implements IntegrationService, Startable,
@@ -648,7 +648,7 @@ public class IntegrationServiceImpl
         catch(Exception e)
         {
             throw new ComponentInitializationError("failed to lookup relation "
-                + SITE_APP_RELATION_NAME);
+                + SITE_APP_RELATION_NAME, e);
         }
         return siteApplications;
     }
@@ -669,7 +669,8 @@ public class IntegrationServiceImpl
             return true;
         }
         Relation siteApplications = getSiteApplicationsRelation(coralSession);
-        if(applicationRes.getEnabled() && siteApplications.hasRef(site, applicationRes))
+        if(applicationRes.getEnabled()
+            && (applicationRes.getRequired() || siteApplications.hasRef(site, applicationRes)))
         {
             return true;
         }
