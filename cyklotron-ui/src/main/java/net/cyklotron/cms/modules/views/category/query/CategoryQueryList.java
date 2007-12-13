dd6392149554b88910a64ef79b9779304d935f9f
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-12-13 15:57:38 +0000

    CYKLO-470 working on queries verbose view

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/category/query/CategoryQueryList.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/category/query/CategoryQueryList.java
@@ -8,6 +8,18 @@ package net.cyklotron.cms.modules.views.category.query;
 
 import org.jcontainer.dna.Logger;
 import org.objectledge.context.Context;
+import org.objectledge.coral.relation.query.parser.ASTIntersectionExpression;
+import org.objectledge.coral.relation.query.parser.ASTInvertedRelationExpression;
+import org.objectledge.coral.relation.query.parser.ASTRelationMapExpression;
+import org.objectledge.coral.relation.query.parser.ASTRelationName;
+import org.objectledge.coral.relation.query.parser.ASTResourceIdentifierId;
+import org.objectledge.coral.relation.query.parser.ASTResourceIdentifierPath;
+import org.objectledge.coral.relation.query.parser.ASTStart;
+import org.objectledge.coral.relation.query.parser.ASTSumExpression;
+import org.objectledge.coral.relation.query.parser.ASTTransitiveRelationMapExpression;
+import org.objectledge.coral.relation.query.parser.RelationQueryParser;
+import org.objectledge.coral.relation.query.parser.RelationQueryParserVisitor;
+import org.objectledge.coral.relation.query.parser.SimpleNode;
 import org.objectledge.coral.session.CoralSession;
 import org.objectledge.coral.store.Resource;
 import org.objectledge.coral.table.CoralTableModel;
@@ -23,40 +35,39 @@ import org.objectledge.web.HttpContext;
 import org.objectledge.web.mvc.MVCContext;
 
 import net.cyklotron.cms.CmsDataFactory;
+import net.cyklotron.cms.category.query.CategoryQueryResource;
 import net.cyklotron.cms.category.query.CategoryQueryService;
 import net.cyklotron.cms.modules.views.BaseCMSScreen;
 import net.cyklotron.cms.preferences.PreferencesService;
 import net.cyklotron.cms.site.SiteResource;
 
 /**
- * @author fil
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ * Displays a list of defined category queries.
  */
-public class CategoryQueryList extends BaseCMSScreen
+public class CategoryQueryList
+    extends BaseCMSScreen
 {
-    protected CategoryQueryService categoryQueryService;
-    
-    
-    
+    private final CategoryQueryService categoryQueryService;
+
     public CategoryQueryList(org.objectledge.context.Context context, Logger logger,
         PreferencesService preferencesService, CmsDataFactory cmsDataFactory,
         TableStateManager tableStateManager, CategoryQueryService categoryQueryService)
     {
-        super(context, logger, preferencesService, cmsDataFactory, tableStateManager);
-        
+        super(context, logger, preferencesService, cmsDataFactory, tableStateManager);        
         this.categoryQueryService = categoryQueryService;
     }
-    
-    public void process(Parameters parameters, MVCContext mvcContext, TemplatingContext templatingContext, HttpContext httpContext, I18nContext i18nContext, CoralSession coralSession) 
+
+    public void process(Parameters parameters, MVCContext mvcContext,
+        TemplatingContext templatingContext, HttpContext httpContext, I18nContext i18nContext,
+        CoralSession coralSession)
         throws ProcessingException
     {
         SiteResource site = getSite();
         try
         {
             Resource queryRoot = categoryQueryService.getCategoryQueryRoot(coralSession, site);
-            TableState state = tableStateManager.getState(context, "cms:category,query,CategoryQueryList:"+site.getIdString());
+            TableState state = tableStateManager.getState(context,
+                "cms:category,query,CategoryQueryList:" + site.getIdString());
             if(state.isNew())
             {
                 state.setRootId(queryRoot.getIdString());
@@ -69,26 +80,125 @@ public class CategoryQueryList extends BaseCMSScreen
             templatingContext.put("table", table);
             boolean verbose = parameters.getBoolean("verbose", false);
             templatingContext.put("verbose", verbose);
-            if(verbose) 
-            {
-            	parseQueries(queryRoot, templatingContext, coralSession);
-            }
+            templatingContext.put("queryParser", new QueryParserTool(coralSession));
         }
         catch(Exception e)
         {
-            throw new ProcessingException("failed to retrieve information", e);    
+            throw new ProcessingException("failed to retrieve information", e);
         }
     }
-    
+
     public boolean checkAccessRights(Context context)
         throws ProcessingException
     {
         CoralSession coralSession = (CoralSession)context.getAttribute(CoralSession.class);
         return checkPermission(context, coralSession, "cms.category.query.modify");
     }
-    
-    private void parseQueries(Resource queryRoot, TemplatingContext templatingContext, CoralSession coralSession) 
+
+    public class QueryParserTool
     {
-    	Resource[] queries = queryRoot.getChildren();
+        private final CoralSession coralSession;
+
+        public QueryParserTool(CoralSession coralSession)
+        {
+            this.coralSession = coralSession;
+        }
+
+        public String parse(CategoryQueryResource queryResource)
+            throws Exception
+        {
+            SimpleNode queryAST = RelationQueryParser.executeParse(queryResource.getQuery());
+            RelationQueryParserVisitor visitor = new RelationQueryParserVisitor()
+                {
+
+                    public Object visit(SimpleNode node, Object data)
+                    {
+                        return node.childrenAccept(this, data);
+                    }
+
+                    public Object visit(ASTStart node, Object data)
+                    {
+                        return visit((SimpleNode)node, data);
+                    }
+
+                    private Object visitComposite(SimpleNode node, Object data, char op)
+                    {
+                        StringBuilder buff = new StringBuilder();
+                        for (int i = 0; i < node.jjtGetNumChildren(); i++)
+                        {
+                            buff.append((String)node.jjtGetChild(i).jjtAccept(this, data));
+                            if(i < node.jjtGetNumChildren() - 1)
+                            {
+                                buff.append(' ').append(op).append(' ');
+                            }
+                        }
+                        if(node.jjtGetNumChildren() > 1 && node.jjtGetParent() != null)
+                        {
+                            buff.insert(0, '(');
+                            buff.append(')');
+                        }
+                        return buff.toString();
+                    }
+
+                    public Object visit(ASTSumExpression node, Object data)
+                    {
+                        return visitComposite(node, data, '|');
+                    }
+
+                    public Object visit(ASTIntersectionExpression node, Object data)
+                    {
+                        return visitComposite(node, data, '|');
+                    }
+
+                    public Object visit(ASTRelationMapExpression node, Object data)
+                    {
+                        return visit((SimpleNode)node, data);
+                    }
+
+                    public Object visit(ASTTransitiveRelationMapExpression node, Object data)
+                    {
+                        return visit((SimpleNode)node, data);
+                    }
+
+                    public Object visit(ASTInvertedRelationExpression node, Object data)
+                    {
+                        return visit((SimpleNode)node, data);
+                    }
+
+                    public Object visit(ASTRelationName node, Object data)
+                    {
+                        return visit((SimpleNode)node, data);
+                    }
+
+                    public Object visit(ASTResourceIdentifierId node, Object data)
+                    {
+                        try
+                        {
+                            return coralSession.getStore().getResource(
+                                Long.parseLong(node.getIdentifier())).getName();
+                        }
+                        catch(Exception e)
+                        {
+                            throw new RuntimeException("Invalid category resource id in query", e);
+                        }
+                    }
+
+                    public Object visit(ASTResourceIdentifierPath node, Object data)
+                    {
+                        try
+                        {
+                            return coralSession.getStore().getUniqueResourceByPath(
+                                node.getIdentifier()).getName();
+                        }
+                        catch(Exception e)
+                        {
+                            throw new RuntimeException("Invalid category resource path in query", e);
+
+                        }
+                    }
+
+                };
+            return (String)visitor.visit(queryAST, null);
+        }
     }
 }
