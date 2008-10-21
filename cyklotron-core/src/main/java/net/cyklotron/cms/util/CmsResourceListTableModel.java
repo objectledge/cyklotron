4c3093af8601ea098881aa438cf282a0a0715be2
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2008-10-21 14:44:24 +0000

    CYKLO-525:  got rid of the problematic casting. fixed some generics warnings

--- cyklotron-core/src/main/java/net/cyklotron/cms/util/CmsResourceListTableModel.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/util/CmsResourceListTableModel.java
@@ -1,5 +1,6 @@
 package net.cyklotron.cms.util;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -19,7 +20,7 @@ import net.cyklotron.cms.structure.table.ValidityStartComparator;
  * Implementation of Table model for CMS resources
  *
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: CmsResourceListTableModel.java,v 1.6 2008-10-07 16:46:43 rafal Exp $
+ * @version $Id: CmsResourceListTableModel.java,v 1.7 2008-10-21 14:44:18 rafal Exp $
  */
 public class CmsResourceListTableModel extends ResourceListTableModel
 {
@@ -31,16 +32,11 @@ public class CmsResourceListTableModel extends ResourceListTableModel
         Resource[] array, Locale locale)
         throws TableException
     {
-        super(array, locale);
-        this.integrationService = integrationService;
-        this.context = context;
-        // this should help with null context and integration service...
-        // later should be refactored and done in correct way
-        columns = getColumns(locale, array);
+        this(context, integrationService, Arrays.asList(array), locale);
     }
 
     public CmsResourceListTableModel(Context context, IntegrationService integrationService,
-        List list, Locale locale)
+        List<Resource> list, Locale locale)
         throws TableException
     {
         super(list, locale);
@@ -48,24 +44,24 @@ public class CmsResourceListTableModel extends ResourceListTableModel
         this.context = context;
         // this should help with null context and integration service...
         // later should be refactored and done in correct way
-        columns = getColumns(locale, (Resource[])list.toArray());
+        columns = getColumns(locale, list);
     }
 
-    protected TableColumn[] getColumns(Locale locale, Resource[] array)
+    protected TableColumn<Resource>[] getColumns(Locale locale, List<Resource> list)
         throws TableException
     {
-        TableColumn[] cols = super.getColumns(locale, array);
-        TableColumn[] newCols = new TableColumn[cols.length + 6];
+        TableColumn<Resource>[] cols = super.getColumns(locale, list);
+        TableColumn<Resource>[] newCols = new TableColumn[cols.length + 6];
         for(int i=0; i<cols.length; i++)
         {
             newCols[i] = cols[i];
         }
-        newCols[cols.length] = new TableColumn("index.title", new IndexTitleComparator(context, integrationService, locale));
-		newCols[cols.length + 1] = new TableColumn("priority", new PriorityComparator());
-		newCols[cols.length + 2] = new TableColumn("validity.start", new ValidityStartComparator());
-		newCols[cols.length + 3] = new TableColumn("priority.validity.start", new PriorityAndValidityStartComparator());
-        newCols[cols.length + 4] = new TableColumn("event.start", new EventStartComparator());
-        newCols[cols.length + 5] = new TableColumn("event.end", new EventEndComparator());
+        newCols[cols.length] = new TableColumn<Resource>("index.title", new IndexTitleComparator(context, integrationService, locale));
+		newCols[cols.length + 1] = new TableColumn<Resource>("priority", new PriorityComparator());
+		newCols[cols.length + 2] = new TableColumn<Resource>("validity.start", new ValidityStartComparator());
+		newCols[cols.length + 3] = new TableColumn<Resource>("priority.validity.start", new PriorityAndValidityStartComparator());
+        newCols[cols.length + 4] = new TableColumn<Resource>("event.start", new EventStartComparator());
+        newCols[cols.length + 5] = new TableColumn<Resource>("event.end", new EventEndComparator());
         return newCols;
     }
 }
