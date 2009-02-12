1b96c83813c71910c929bd184de581c747c768f5
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-02-12 17:13:12 +0000

    CYKLO-557:  added a method that builds description: site and roleResource for an arbitrary role using Coral query

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/security/SecurityServiceHelper.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/security/SecurityServiceHelper.java
@@ -3,6 +3,14 @@
  */
 package net.cyklotron.cms.modules.views.security;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.objectledge.coral.security.Role;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.store.Resource;
+
 import net.cyklotron.cms.security.CmsSecurityException;
 import net.cyklotron.cms.security.RoleResource;
 import net.cyklotron.cms.security.SecurityService;
@@ -10,15 +18,47 @@ import net.cyklotron.cms.security.SecurityService;
 public class SecurityServiceHelper
 {
     private final SecurityService cmsSecurityService;
-    
-    public SecurityServiceHelper(SecurityService cmsSecurityService)
+
+    private final CoralSession coralSession;
+
+    public SecurityServiceHelper(SecurityService cmsSecurityService, CoralSession coralSession)
     {
         this.cmsSecurityService = cmsSecurityService;
+        this.coralSession = coralSession;
     }
-    
+
     public String getGroupName(RoleResource roleResource)
         throws CmsSecurityException
     {
         return cmsSecurityService.getShortGroupName(roleResource);
+    }
+
+    public Map<String, Object> getRoleDescription(Role role)
+        throws Exception
+    {
+        Map<String, Object> result = new HashMap<String, Object>();
+        List<Resource> qRes = coralSession.getQuery().executeQuery(
+            "FIND RESOURCE FROM cms.security.role WHERE role = " + role.getIdString()).getList(1);
+        if(qRes.size() > 1)
+        {
+            result.put("special", "ambiguous");
+        }
+        else if(qRes.size() == 0)
+        {
+            result.put("special", "system");
+        }
+        else
+        {
+            result.put("role", qRes.get(0));
+            for(Resource r = qRes.get(0).getParent(); r != null ; r = r.getParent())
+            {
+                if(r.getResourceClass().getName().equals("site.site"))
+                {
+                    result.put("site", r);
+                    break;
+                }
+            }                
+        }
+        return result;
     }
 }
