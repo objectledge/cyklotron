b4c85eaa64d8dc171429ef57890752a46a7c23b6
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-02-12 12:07:53 +0000

    CYKLO-556:  a method for checking if the current user is allowed to modify the current node

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/structure/BaseStructureAction.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/structure/BaseStructureAction.java
@@ -1,7 +1,11 @@
 package net.cyklotron.cms.modules.actions.structure;
 
 import org.jcontainer.dna.Logger;
+import org.objectledge.context.Context;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.pipeline.ProcessingException;
 
+import net.cyklotron.cms.CmsData;
 import net.cyklotron.cms.CmsDataFactory;
 import net.cyklotron.cms.modules.actions.BaseCMSAction;
 import net.cyklotron.cms.structure.NaviConstants;
@@ -25,6 +29,27 @@ public abstract class BaseStructureAction
     {
         super(logger, structureService, cmsDataFactory);
         this.styleService = styleService;
+    }
+
+    /**
+     * Checks if the current subject can modify the current node.
+     * <p>
+     * Checks if the user has cms.structure.modify permission on the node, or is node owner and has
+     * cms.structure.modify_own permission.
+     * </p>
+     * 
+     * @param context Context
+     * @return true if the current subject can modify the current node.
+     * @throws ProcessingException if checking fails.
+     */
+    protected boolean checkModifyPermission(Context context)
+        throws ProcessingException
+    {
+        CoralSession coralSession = context.getAttribute(CoralSession.class);
+        CmsData cmsData = cmsDataFactory.getCmsData(context);
+        boolean isOwner = cmsData.getNode().getOwner().equals(coralSession.getUserSubject());
+        return checkPermission(context, coralSession, "cms.structure.modify") || isOwner
+            && checkPermission(context, coralSession, "cms.structure.modify_own");
     }
 }
 
