60dc350ed462a72e2da904d3710fed614aae675c
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-02-12 14:42:04 +0000

    CYKLO-557:  return only direct assignments of the role

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/security/RoleAssignments.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/security/RoleAssignments.java
@@ -15,6 +15,7 @@ import org.objectledge.coral.entity.Entity;
 import org.objectledge.coral.entity.EntityDoesNotExistException;
 import org.objectledge.coral.entity.EntityFactory;
 import org.objectledge.coral.security.Role;
+import org.objectledge.coral.security.RoleAssignment;
 import org.objectledge.coral.security.RoleImplication;
 import org.objectledge.coral.security.Subject;
 import org.objectledge.coral.session.CoralSession;
@@ -225,10 +226,10 @@ public class RoleAssignments
     public static Set<Subject> getAssignedSubjects(Role role)
     {
         Set<Subject> subjects = new HashSet<Subject>();
-        Subject assigned[] = role.getSubjects();
-        for (int i = 0; i < assigned.length; i++)
+        RoleAssignment assigned[] = role.getRoleAssignments();
+        for (RoleAssignment a : assigned)
         {
-            subjects.add(assigned[i]);
+            subjects.add(a.getSubject());
         }
         return subjects;
     }
