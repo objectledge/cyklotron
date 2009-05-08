96c1a1922de95b9f62f7644051b60da905e36ee4
Łukasz Urbański <lukasz.urbanski@caltha.pl>
2009-05-08 09:39:48 +0000

    CYKLO - 520: Revert relation save procedure simplified.

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/related/UpdateRelations.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/related/UpdateRelations.java
@@ -9,6 +9,8 @@ import java.util.Map;
 import org.jcontainer.dna.Logger;
 import org.objectledge.context.Context;
 import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.relation.Relation;
+import org.objectledge.coral.relation.RelationModification;
 import org.objectledge.coral.session.CoralSession;
 import org.objectledge.coral.store.Resource;
 import org.objectledge.coral.util.CoralEntitySelectionState;
@@ -54,61 +56,41 @@ public class UpdateRelations
         boolean symetricRelation = parameters.getBoolean("sym_rel", false);
         try
         {
+            Relation relation = relatedService.getRelation(coralSession);
+            RelationModification modification = new RelationModification();
+            
             Resource resource = coralSession.getStore().getResource(resId);
             ResourceSelectionState relatedState = ResourceSelectionState.getState(context,
                 RELATED_SELECTION_STATE + ":" + resource.getIdString());
             relatedState.update(parameters);
+           
+            ListIterator<Resource> iterator;
+            List<Resource> oldResorces = new ArrayList<Resource>(Arrays.asList(relation.get(resource)));
+         
+            List<Resource> toRemove = new ArrayList<Resource>(oldResorces);
+            List<Resource> toAdd = new ArrayList<Resource>(
+                            relatedState.getEntities(coralSession,"selected").keySet());
             
             if(symetricRelation){
-               
-                ListIterator<Resource> iterator;
-                List<Resource> oldResorces = new ArrayList<Resource>(
-                                   Arrays.asList(relatedService.getRelatedTo(coralSession, resource, null, null)));
-                
-                List<Resource> toRemove = new ArrayList<Resource>(oldResorces);
-                List<Resource> toAdd = new ArrayList<Resource>(
-                                relatedState.getEntities(coralSession,"selected").keySet());
                 
                 toRemove.removeAll(toAdd);
                 toAdd.removeAll(oldResorces);
-               
-                iterator = toAdd.listIterator();
-                while(iterator.hasNext()){
-                    
-                   Resource newResource = (Resource)iterator.next();
-                   ResourceSelectionState relatedStateSym = ResourceSelectionState.getState(context,
-                        RELATED_SELECTION_STATE + ":" + newResource.getIdString());
-                   relatedStateSym.setValue(resource, "selected");
                 
-                   Map selected = relatedStateSym.getEntities(coralSession,"selected");
-                   Resource[] resources = new Resource[selected.size()];
-                   selected.keySet().toArray(resources);
-                   
-                   relatedService.setRelatedTo(coralSession, newResource, resources);
-                   CoralEntitySelectionState.removeState(context, relatedStateSym);
-                }
-                
+                modification.add(toAdd, resource);                
+                modification.add(resource, toAdd);
                 iterator = toRemove.listIterator();
                 while(iterator.hasNext()){
-                    
-                   Resource newResource = (Resource)iterator.next();
-                   ResourceSelectionState relatedStateSym = ResourceSelectionState.getState(context,
-                        RELATED_SELECTION_STATE + ":" + newResource.getIdString());
-                   relatedStateSym.remove(resource);
-                
-                   Map selected = relatedStateSym.getEntities(coralSession,"selected");
-                   Resource[] resources = new Resource[selected.size()];
-                   selected.keySet().toArray(resources);
-                   
-                   relatedService.setRelatedTo(coralSession, newResource, resources);
-                   CoralEntitySelectionState.removeState(context, relatedStateSym);
+                    Resource removeResource = (Resource)iterator.next();
+                    modification.remove(removeResource, resource);
+                    modification.remove(resource, removeResource);
                 }
             }
+            else{
+                modification.clear();
+                modification.add(resource, toAdd);
+            }
             
-            Map selected = relatedState.getEntities(coralSession,"selected");
-            Resource[] resources = new Resource[selected.size()];
-            selected.keySet().toArray(resources);
-            relatedService.setRelatedTo(coralSession, resource, resources);
+            coralSession.getRelationManager().updateRelation(relation, modification);
             CoralEntitySelectionState.removeState(context, relatedState);
         }
         catch(EntityDoesNotExistException e)
