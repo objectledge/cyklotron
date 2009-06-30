ecf932c75f52e51ded90e4722ca24dc057b76882
Łukasz Urbański <lukasz.urbanski@caltha.pl>
2009-06-30 13:34:49 +0000

    CYKLO-538: attachments save section added to SaveProposedChanges action.

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/structure/SaveProposedChanges.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/structure/SaveProposedChanges.java
@@ -5,44 +5,31 @@ import static net.cyklotron.cms.documents.DocumentMetadataHelper.dom4jToText;
 import static net.cyklotron.cms.documents.DocumentMetadataHelper.elm;
 import static net.cyklotron.cms.documents.DocumentMetadataHelper.selectFirstText;
 import static net.cyklotron.cms.documents.DocumentMetadataHelper.textToDom4j;
-import static net.cyklotron.cms.structure.internal.ProposedDocumentData.getAttachmentName;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-
-import javax.xml.crypto.Data;
 
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.jcontainer.dna.Logger;
 import org.objectledge.context.Context;
-import org.objectledge.coral.datatypes.ResourceList;
 import org.objectledge.coral.entity.EntityDoesNotExistException;
 import org.objectledge.coral.relation.Relation;
 import org.objectledge.coral.relation.RelationModification;
 import org.objectledge.coral.security.Permission;
-import org.objectledge.coral.security.Subject;
 import org.objectledge.coral.session.CoralSession;
 import org.objectledge.coral.session.CoralSessionFactory;
 import org.objectledge.coral.store.Resource;
 import org.objectledge.coral.store.ValueRequiredException;
 import org.objectledge.html.HTMLException;
-import org.objectledge.html.HTMLService;
 import org.objectledge.parameters.Parameters;
-import org.objectledge.parameters.RequestParameters;
 import org.objectledge.pipeline.ProcessingException;
 import org.objectledge.templating.TemplatingContext;
 import org.objectledge.upload.FileUpload;
-import org.objectledge.upload.UploadContainer;
-import org.objectledge.upload.UploadLimitExceededException;
 import org.objectledge.utils.StackTrace;
-import org.objectledge.utils.StringUtils;
 import org.objectledge.web.HttpContext;
 import org.objectledge.web.mvc.MVCContext;
 
@@ -52,16 +39,10 @@ import net.cyklotron.cms.category.CategoryResource;
 import net.cyklotron.cms.category.CategoryService;
 import net.cyklotron.cms.documents.DocumentNodeResource;
 import net.cyklotron.cms.documents.DocumentNodeResourceImpl;
-import net.cyklotron.cms.files.DirectoryResource;
-import net.cyklotron.cms.files.DirectoryResourceImpl;
-import net.cyklotron.cms.files.FileResource;
 import net.cyklotron.cms.files.FilesService;
 import net.cyklotron.cms.modules.views.documents.BaseSkinableDocumentScreen;
 import net.cyklotron.cms.related.RelatedService;
-import net.cyklotron.cms.structure.NavigationNodeAlreadyExistException;
-import net.cyklotron.cms.structure.NavigationNodeResource;
 import net.cyklotron.cms.structure.NavigationNodeResourceImpl;
-import net.cyklotron.cms.structure.StructureException;
 import net.cyklotron.cms.structure.StructureService;
 import net.cyklotron.cms.structure.internal.ProposedDocumentData;
 import net.cyklotron.cms.style.StyleService;
@@ -78,14 +59,9 @@ public class SaveProposedChanges
     extends BaseAddEditNodeAction
 {
     private CategoryService categoryService;
+    
+    private RelatedService relatedService;
 
-    private final FileUpload uploadService;
-
-    private final FilesService filesService;
-
-    private final CoralSessionFactory coralSessionFactory;
-
-    private final RelatedService relatedService;
 
     public SaveProposedChanges(Logger logger, StructureService structureService,
         CmsDataFactory cmsDataFactory, StyleService styleService, CategoryService categoryService,
@@ -94,9 +70,6 @@ public class SaveProposedChanges
     {
         super(logger, structureService, cmsDataFactory, styleService);
         this.categoryService = categoryService;
-        this.uploadService = uploadService;
-        this.filesService = filesService;
-        this.coralSessionFactory = coralSessionFactory;
         this.relatedService = relatedService;
     }
 
@@ -158,8 +131,8 @@ public class SaveProposedChanges
                     node.setValidityEnd(proposedData.getValidityEnd());
                 }
 
-           //     if(parameters.getBoolean("updateMetaData", false))
-           //     {
+                if(parameters.getBoolean("metaData", false))
+                {
 
                     Document metaDom = textToDom4j(node.getMeta());
 
@@ -226,7 +199,7 @@ public class SaveProposedChanges
 
                     Document doc = doc(metaElm);
                     node.setMeta(dom4jToText(doc));
-           //     }
+                }
 
                 if(parameters.getBoolean("selectedCategories", false))
                 {                       
@@ -239,7 +212,7 @@ public class SaveProposedChanges
                     // take proposed document categories
                     Set<CategoryResource> proposedDocCategories = proposedData.getSelectedCategories();
                     
-                 // take component available root categories id 
+                    // take component available root categories id 
                     long root_category_1 = screenConfig.getLong("category_id_1", -1);  
                     long root_category_2 = screenConfig.getLong("category_id_2", -1);
                     List<CategoryResource> allAvailableCategories = new ArrayList<CategoryResource>();
@@ -263,21 +236,50 @@ public class SaveProposedChanges
                     coralSession.getRelationManager().updateRelation(relation, modification);
 
                 }
+                if(parameters.getBoolean("attachments", false))
+                {
+                    Relation relation = categoryService.getResourcesRelation(coralSession);
+                    RelationModification modification = new RelationModification();
+                    
+                    List<Resource> publishedDocAttachments = new ArrayList<Resource>(Arrays
+                        .asList(relatedService.getRelatedTo(coralSession, node, 
+                            node.getRelatedResourcesSequence(), null)));
+                    
+                    List<Resource> proposedDocAttachments = proposedData.getAttachments();                   
+                   
+                    List<Resource> toRemove = new ArrayList<Resource>(publishedDocAttachments);
+                    List<Resource> toAdd = new ArrayList<Resource>(proposedDocAttachments);
+                    
+                    toRemove.removeAll(proposedDocAttachments);
+                    toAdd.removeAll(publishedDocAttachments);
+                    
+                    modification.add(node, toAdd);
+                    modification.remove(node, toRemove);
+
+                    coralSession.getRelationManager().updateRelation(relation, modification);
+                }
                 
+                node.setProposedContent(null);
             }
 
         }
         catch(EntityDoesNotExistException e)
         {
-            e.printStackTrace();
+            logger.error("excception", e);
+            templatingContext.put("result", "exception");
+            templatingContext.put("trace", new StackTrace(e));
         }
         catch(ValueRequiredException e)
         {
-            e.printStackTrace();
+            logger.error("excception", e);
+            templatingContext.put("result", "exception");
+            templatingContext.put("trace", new StackTrace(e));
         }
         catch(HTMLException e)
         {
-            e.printStackTrace();
+            logger.error("excception", e);
+            templatingContext.put("result", "exception");
+            templatingContext.put("trace", new StackTrace(e));
         }
     }
 
