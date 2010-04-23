49c2f72b9b6b66159fb9b1562a81b0acf1632b82
Łukasz Urbański <lukasz.urbanski@caltha.pl>
2010-04-23 09:27:36 +0000

    CYKLO-718: long queries section added to category query identifiers fix action.

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/fixes/ConvertCategoryQueryIdentifiers.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/fixes/ConvertCategoryQueryIdentifiers.java
@@ -32,9 +32,11 @@ public class ConvertCategoryQueryIdentifiers
     extends BaseCoralView
 {
     private CategoryQueryService categoryQueryService;
+
     private IntegrationService integrationService;
-    
-    public ConvertCategoryQueryIdentifiers(Context context,CategoryQueryService categoryQueryService,IntegrationService integrationService)
+
+    public ConvertCategoryQueryIdentifiers(Context context,
+        CategoryQueryService categoryQueryService, IntegrationService integrationService)
     {
         super(context);
         this.categoryQueryService = categoryQueryService;
@@ -47,77 +49,85 @@ public class ConvertCategoryQueryIdentifiers
         throws ProcessingException
     {
         try
-        {            
-            Resource[] resources = coralSession.getQuery().executeQuery("FIND RESOURCE FROM category.query").getArray(1);
-            CategoryResolver resolver = categoryQueryService.getCategoryResolver(); //added
-            
-            Map<CategoryQueryResource, Set<String>> brokenQueries = new HashMap<CategoryQueryResource, Set<String>>(); //added
+        {
+            Resource[] resources = coralSession.getQuery().executeQuery(
+                "FIND RESOURCE FROM category.query").getArray(1);
+            CategoryResolver resolver = categoryQueryService.getCategoryResolver(); // added
+
+            Set<CategoryQueryResource> unfixedQueries = new HashSet<CategoryQueryResource>(); // added
+            Map<CategoryQueryResource, Set<String>> brokenQueries = new HashMap<CategoryQueryResource, Set<String>>(); // added
             Set<CategoryQueryResource> queryWithDefIdentifiers = new HashSet<CategoryQueryResource>();
             Set<CategoryQueryResource> queryWithoutDefIdentifiers = new HashSet<CategoryQueryResource>();
-            
+
             for(Resource resource : resources)
             {
-              CategoryQueryResource queryResource = (CategoryQueryResource)resource;
-                
-              // test for broken categories
-              String[] optCategoryIds = CategoryQueryUtil.splitCategoryIdentifiers(queryResource.getOptionalCategoryIdentifiers());
-              String[] reqCategoryIds  = CategoryQueryUtil.splitCategoryIdentifiers(queryResource.getRequiredCategoryIdentifiers());
-              Set<String> actCatIds = new HashSet<String>();
-              Set<String> brokenCatIds = new HashSet<String>();
-                  
-              for (String catIds : optCategoryIds)
-              {
-                  if(resolver.resolveCategoryIdentifier(catIds) == null)
-                  {
-                      brokenCatIds.add(catIds);
-                  }
-              }
-              for (String catIds : reqCategoryIds)
-              {
-                  if(resolver.resolveCategoryIdentifier(catIds) == null)
-                  {
-                      brokenCatIds.add(catIds);
-                  }
-              }
+                CategoryQueryResource queryResource = (CategoryQueryResource)resource;
 
-              if(!brokenCatIds.isEmpty()){
-                 brokenQueries.put(queryResource, brokenCatIds);
-              }
-              
-              // verify categories identifiers
-              if(queryResource.getUseIdsAsIdentifiers(false)){
-                   
-                   if(!brokenCatIds.isEmpty())
-                   {
-                       CategoryQueryResourceData queryData = new CategoryQueryResourceData();
-                       queryData.init(coralSession, queryResource, categoryQueryService, integrationService);
-                       CategoryQueryBuilder parsedQuery = new CategoryQueryBuilder(coralSession, queryData.getCategoriesSelection(), true);
-                       queryResource.setUseIdsAsIdentifiers(true);
-                       queryResource.setQuery(parsedQuery.getQuery());
-                       queryResource.setRequiredCategoryIdentifiers(CategoryQueryUtil.joinCategoryIdentifiers(parsedQuery.getRequiredIdentifiers()));
-                       queryResource.setOptionalCategoryIdentifiers(CategoryQueryUtil.joinCategoryIdentifiers(parsedQuery.getOptionalIdentifiers()));
-                       queryResource.update();
-                   }
-                   queryWithDefIdentifiers.add(queryResource);
-                    
-              }else{
-                    /*
-                     * update query to replace category name path to category id
-                     */ 
-                   CategoryQueryResourceData queryData = new CategoryQueryResourceData();
-                   queryData.init(coralSession, queryResource, categoryQueryService, integrationService);
-                   CategoryQueryBuilder parsedQuery = new CategoryQueryBuilder(coralSession, queryData.getCategoriesSelection(), true);
-                   queryResource.setUseIdsAsIdentifiers(true);
-                   queryResource.setQuery(parsedQuery.getQuery());
-                   queryResource.setRequiredCategoryIdentifiers(CategoryQueryUtil.joinCategoryIdentifiers(parsedQuery.getRequiredIdentifiers()));
-                   queryResource.setOptionalCategoryIdentifiers(CategoryQueryUtil.joinCategoryIdentifiers(parsedQuery.getOptionalIdentifiers()));
-                   queryResource.update();
-                    
-                   queryWithoutDefIdentifiers.add(queryResource);
-              }
-                
+                // test query type
+                if(queryResource.getSimpleQuery(false)
+                    || !queryResource.isLongQueryDefined())
+                {
+                    // test query for broken categories
+                    String[] optCategoryIds = CategoryQueryUtil
+                        .splitCategoryIdentifiers(queryResource.getOptionalCategoryIdentifiers());
+                    String[] reqCategoryIds = CategoryQueryUtil
+                        .splitCategoryIdentifiers(queryResource.getRequiredCategoryIdentifiers());
+                    Set<String> actCatIds = new HashSet<String>();
+                    Set<String> brokenCatIds = new HashSet<String>();
+
+                    for(String catIds : optCategoryIds)
+                    {
+                        if(resolver.resolveCategoryIdentifier(catIds) == null)
+                        {
+                            brokenCatIds.add(catIds);
+                        }
+                    }
+                    for(String catIds : reqCategoryIds)
+                    {
+                        if(resolver.resolveCategoryIdentifier(catIds) == null)
+                        {
+                            brokenCatIds.add(catIds);
+                        }
+                    }
+
+                    // if query has one or more broken categories
+                    if(!brokenCatIds.isEmpty())
+                    {
+                        brokenQueries.put(queryResource, brokenCatIds);
+                    }
+
+                    // verify categories identifiers
+                    if(queryResource.getUseIdsAsIdentifiers(false))
+                    {
+
+                        if(!brokenCatIds.isEmpty())
+                        {
+                            /*
+                             * fix query excluding broken categories
+                             */
+                            convertQuery(coralSession, queryResource);
+                        }
+                        queryWithDefIdentifiers.add(queryResource);
+
+                    }
+                    else
+                    {
+                        /*
+                         * update query to replace category name path to category id
+                         */
+                        convertQuery(coralSession, queryResource);
+                        queryWithoutDefIdentifiers.add(queryResource);
+                    }
+                }
+                else
+                {
+                    // if query is long type
+                    unfixedQueries.add(queryResource);
+                }
             }
-            templatingContext.put("broken_queries", brokenQueries);
+
+            templatingContext.put("unfixed_queries", unfixedQueries);
+            templatingContext.put("fixed_queries", brokenQueries);
             templatingContext.put("queries_with_ids", queryWithDefIdentifiers);
             templatingContext.put("queries_without_ids", queryWithoutDefIdentifiers);
         }
@@ -132,16 +142,31 @@ public class ConvertCategoryQueryIdentifiers
             templatingContext.put("trace", new StackTrace(e));
         }
     }
-    
+
+    private void convertQuery(CoralSession coralSession, CategoryQueryResource queryResource)
+        throws ProcessingException
+    {
+        CategoryQueryResourceData queryData = new CategoryQueryResourceData();
+        queryData.init(coralSession, queryResource, categoryQueryService, integrationService);
+        CategoryQueryBuilder parsedQuery = new CategoryQueryBuilder(coralSession, queryData
+            .getCategoriesSelection(), true);
+        queryResource.setUseIdsAsIdentifiers(true);
+        queryResource.setQuery(parsedQuery.getQuery());
+        queryResource.setRequiredCategoryIdentifiers(CategoryQueryUtil
+            .joinCategoryIdentifiers(parsedQuery.getRequiredIdentifiers()));
+        queryResource.setOptionalCategoryIdentifiers(CategoryQueryUtil
+            .joinCategoryIdentifiers(parsedQuery.getOptionalIdentifiers()));
+        queryResource.update();
+    }
+
     /**
-     * @{inheritDoc}
+     * @{inheritDoc
      */
     public boolean checkAccessRights(Context context)
         throws Exception
     {
         CoralSession coralSession = (CoralSession)context.getAttribute(CoralSession.class);
-        Role cmsAdministrator = coralSession.getSecurity().
-            getUniqueRole("cms.administrator");
+        Role cmsAdministrator = coralSession.getSecurity().getUniqueRole("cms.administrator");
         return coralSession.getUserSubject().hasRole(cmsAdministrator);
     }
 }
