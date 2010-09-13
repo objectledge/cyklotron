8975d83b110f3cf333213ebd458a619cba79db1b
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-09-13 23:29:18 +0000

    CYKLO-746 comments

--- cyklotron-core/src/main/java/net/cyklotron/cms/ngodatabase/LocationDatabaseService.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/ngodatabase/LocationDatabaseService.java
@@ -4,29 +4,30 @@ import java.io.IOException;
 import java.util.Set;
 
 /**
- *
+ * Provides access to a collection of location descriptors.
+ * 
  * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
  * @version $Id: DocumentService.java,v 1.4 2007-11-18 21:23:07 rafal Exp $
  */
 public interface LocationDatabaseService 
 {
     /**
-     * update Pna data from source.
+     * Update Locations data from source.
      */
     public void update();
     
     /**
-     * get Pna set by Post Code
+     * get Location set by post code
      */
     public Set<Location> getLocationsByPostCode(String postCode);
     
     /**
-     * get Pna set by City
+     * get Location set by city name
      */
     public Set<Location> getLocationsByCity(String city);
     
     /**
-     * get Pna set by Area
+     * get Location set by province name
      */
     public Set<Location> getLocationsByProvince(String area);
 }
