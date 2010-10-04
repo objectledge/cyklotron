64033e6b5da6afb4282b3c0fd4a06cbf1c747345
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-10-04 14:08:02 +0000

    CYKLO-748 converted fix views into a single fix action, used result set iteration instead of list loading, added an action for dumping legacy adress field content to a text file

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/fixes/UpgradeDocumentMetadata214.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/actions/fixes/UpgradeDocumentMetadata214.java
@@ -1,10 +1,7 @@
-package net.cyklotron.cms.modules.views.fixes;
+package net.cyklotron.cms.modules.actions.fixes;
 
-import static net.cyklotron.cms.documents.DocumentMetadataHelper.selectFirstText;
 import static net.cyklotron.cms.documents.DocumentMetadataHelper.elm;
-
-import java.util.HashMap;
-import java.util.Map;
+import static net.cyklotron.cms.documents.DocumentMetadataHelper.selectFirstText;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
@@ -12,50 +9,39 @@ import org.dom4j.Element;
 import org.dom4j.dom.DOMDocument;
 import org.jcontainer.dna.Logger;
 import org.objectledge.context.Context;
-import org.objectledge.coral.modules.views.BaseCoralView;
 import org.objectledge.coral.query.MalformedQueryException;
 import org.objectledge.coral.query.QueryResults;
 import org.objectledge.coral.session.CoralSession;
-import org.objectledge.coral.store.Resource;
-import org.objectledge.i18n.I18nContext;
 import org.objectledge.html.HTMLException;
-import org.objectledge.parameters.Parameters;
 import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.pipeline.Valve;
 import org.objectledge.templating.TemplatingContext;
-import org.objectledge.web.mvc.MVCContext;
 
 import net.cyklotron.cms.documents.DocumentMetadataHelper;
 import net.cyklotron.cms.documents.DocumentNodeResource;
 import net.cyklotron.cms.ngodatabase.Location;
 import net.cyklotron.cms.ngodatabase.LocationDatabaseService;
 
-/**
- * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
- * @version $Id$
- */
-public class ParseEventPlaceField
-    extends BaseCoralView
+public class UpgradeDocumentMetadata214
+    implements Valve
 {
     private Logger logger;
 
     private LocationDatabaseService locationDatabaseService;
 
-    public ParseEventPlaceField(Context context, LocationDatabaseService locationDatabaseService,
-        Logger logger)
+    public UpgradeDocumentMetadata214(LocationDatabaseService locationDatabaseService, Logger logger)
     {
-        super(context);
         this.logger = logger;
         this.locationDatabaseService = locationDatabaseService;
     }
 
-    /**
-     * Performs the action.
-     */
     @Override
-    public void process(Parameters parameters, TemplatingContext templatingContext,
-        MVCContext mvcContext, I18nContext i18nContext, CoralSession coralSession)
+    public void process(Context context)
         throws ProcessingException
     {
+        CoralSession coralSession = context.getAttribute(CoralSession.class);
+        TemplatingContext templatingContext = context.getAttribute(TemplatingContext.class);
+
         QueryResults results;
 
         try
@@ -67,24 +53,36 @@ public class ParseEventPlaceField
         {
             throw new ProcessingException("cannot get 'documents.document_node' resources", e);
         }
-        Resource[] resources = results.getArray(1);
         String organizedAddress;
-        DocumentNodeResource node;
-        Map<Long, String> adresses = new HashMap<Long, String>();
-        Map<Long, Location> locations = new HashMap<Long, Location>();
 
-        for(Resource res : resources)
+        int counter = 0;
+        for(QueryResults.Row row : results)
         {
             try
             {
-                node = (DocumentNodeResource)res;
+                DocumentNodeResource node = (DocumentNodeResource)row.get();
                 if(node.getMeta() == null)
                 {
                     continue;
                 }
                 if(!node.getMeta().trim().isEmpty())
                 {
-                    ConvertMetaDom(node);
+                    Document metaDom = textToDom4j(node.getMeta());
+                    organizedAddress = stripTags(selectFirstText(metaDom,
+                        "/meta/organisation/address"));
+
+                    if(!organizedAddress.trim().isEmpty())
+                    {
+                        convertMetaDom(node, parseOrganizedAddress(organizedAddress));
+                    }
+                    else
+                    {
+                        convertMetaDom(node, new Location("", "", "", ""));
+                    }
+                }
+                if(counter++ % 1000 == 0)
+                {
+                    System.out.println("converted " + counter + " documents");
                 }
             }
             catch(HTMLException e)
@@ -92,26 +90,50 @@ public class ParseEventPlaceField
                 throw new RuntimeException("malformed metadada in resource ", e);
             }
         }
-
-        templatingContext.put("adresses", adresses);
-        templatingContext.put("locations", locations);
+        templatingContext.put("result", "success");
     }
 
-    private void ConvertMetaDom(DocumentNodeResource node)
+    private Location parseOrganizedAddress(String organizedAddress)
+    {
+        String[] fields = organizedAddress.replaceAll("\\s*[-]\\s*", "-").replaceAll(",", " ")
+            .split("\\s+");
+        String province = "";
+        String city = "";
+        String postCode = "";
+        String street = "";
+
+        for(String field : fields)
+        {
+            if(!field.isEmpty())
+            {
+                if(locationDatabaseService.containsCity(field.substring(0, 1).toUpperCase()
+                    + field.substring(1).toLowerCase()))
+                {
+                    city = field;
+                }
+                else if(locationDatabaseService.containsPostCode(field))
+                {
+                    postCode = field;
+                }
+                else
+                {
+                    street += field + " ";
+                }
+            }
+        }
+        return new Location(province, city, street.trim(), postCode);
+    }
+
+    private void convertMetaDom(DocumentNodeResource node, Location location)
         throws HTMLException
     {
 
         Document doc = textToDom4j(node.getMeta());
-        String organizedBy = selectFirstText(doc, "/meta/organisation/name");        
-        String organizedProvince = selectFirstText(doc, "/meta/organisation/address/province");
-        String organizedPostCode = selectFirstText(doc, "/meta/organisation/address/postcode");
-        String organizedCity = selectFirstText(doc, "/meta/organisation/address/city");
-        String organizedStreet = selectFirstText(doc, "/meta/organisation/address/street");
+        String organizedBy = selectFirstText(doc, "/meta/organisation/name");
         String organizedPhone = selectFirstText(doc, "/meta/organisation/tel");
         String organizedFax = selectFirstText(doc, "/meta/organisation/fax");
         String organizedEmail = selectFirstText(doc, "/meta/organisation/e-mail");
         String organizedWww = selectFirstText(doc, "/meta/organisation/url");
-        String organizedId = selectFirstText(doc, "/meta/organisation/id");
         String sourceName = selectFirstText(doc, "/meta/sources/source/name");
         String sourceUrl = selectFirstText(doc, "/meta/sources/source/url");
         String proposerCredentials = selectFirstText(doc, "/meta/authors/author/name");
@@ -119,13 +141,13 @@ public class ParseEventPlaceField
 
         Element element = elm("meta", elm("authors", elm("author",
             elm("name", proposerCredentials), elm("e-mail", proposerEmail))), elm("sources", elm(
-            "source", elm("name", sourceName), elm("url", sourceUrl))), elm("editor"), elm(
-            "event", elm("address", elm("street", ""), elm("postcode", ""), elm("city", ""),
-                elm("province", ""))), elm("organisation", elm("name", organizedBy), elm("address",
-            elm("street", organizedStreet), elm("postcode", organizedPostCode), elm("city",
-                organizedCity), elm("province", organizedProvince)), elm("tel", organizedPhone),
-            elm("fax", organizedFax), elm("e-mail", organizedEmail), elm("url", organizedWww), elm(
-                "id", organizedId)));
+            "source", elm("name", sourceName), elm("url", sourceUrl))), elm("editor"), elm("event",
+            elm("address", elm("street", ""), elm("postcode", ""), elm("city", ""), elm("province",
+                ""))), elm("organisation", elm("name", organizedBy), elm("address", elm("street",
+            location.getStreet()), elm("postcode", location.getPostCode()), elm("city", location
+            .getCity()), elm("province", location.getProvince())), elm("tel", organizedPhone), elm(
+            "fax", organizedFax), elm("e-mail", organizedEmail), elm("url", organizedWww), elm(
+            "id", "0")));
 
         Document convertedDoc = DocumentMetadataHelper.doc(element);
         String metaDom = DocumentMetadataHelper.dom4jToText(convertedDoc);
