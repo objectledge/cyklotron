70bf63d7b15d266b0e249c8d96cf167a8417d7e8
Łukasz Urbański <lukasz.urbanski@caltha.pl>
2010-10-22 13:22:40 +0000

    CYKLO-750: email address fields at proposeDocumentAddDocument and EditDocument formatter.

--- cyklotron-ui/src/main/resources/content/script/urlsformatter.js
+++ cyklotron-ui/src/main/resources/content/script/urlsformatter.js
@@ -8,12 +8,25 @@ function formatUrlString(urls)
     return "http://" + urls.replace(/http:\/\//ig,"").replace(/[^a-zA-Z0-9./:&?=%]/g," ").replace(/\s\s*/g," http://").trim();
 }
 
+function formatEmailString(emails)
+{  
+    return emails.replace(/[,]/," ").replace(/\s\s*/g,"; ").trim();
+}
+
 /*
  * method convert url field to fit FormTool schema format. 
  */
 function convertUrlFiled(field)
 {
   if(field && field.value.trim() != '') { field.value = formatUrlString(field.value.trim()); }
+}
+
+/*
+ * method convert email field to fit FormTool schema format. 
+ */
+function convertEmailFiled(field)
+{
+  if(field && field.value.trim() != '') { field.value = formatEmailString(field.value.trim()); }
 }
 
 /*
@@ -25,6 +38,8 @@ function convertProposeDocumentUrlFileds(maxOrgs)
   for(var index=1; index<=maxOrgs;index++)
   { 
 	convertUrlFiled(document.getElementById('organization_'+ index + '_www'));
+	convertEmailFiled(document.getElementById('organization_'+ index + '_email'));
   }
   convertUrlFiled(document.getElementById('source_url'));
+  convertEmailFiled(document.getElementById('proposer_email'));
 }
