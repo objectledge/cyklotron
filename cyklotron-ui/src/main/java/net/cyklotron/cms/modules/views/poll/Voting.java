a5d6003389d432ab2bedf00330526920f667a122
Łukasz Urbański <lukasz.urbanski@caltha.pl>
2011-03-07 16:32:46 +0000

    CYKLO-791: PrepareDefault and PrepareResults methods in Voting.

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/poll/Voting.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/poll/Voting.java
@@ -1,13 +1,14 @@
 package net.cyklotron.cms.modules.views.poll;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.jcontainer.dna.Logger;
 import org.objectledge.context.Context;
-import org.objectledge.coral.entity.EntityDoesNotExistException;
 import org.objectledge.coral.session.CoralSession;
-import org.objectledge.coral.store.Resource;
-import org.objectledge.i18n.I18nContext;
 import org.objectledge.parameters.Parameters;
 import org.objectledge.parameters.RequestParameters;
 import org.objectledge.pipeline.ProcessingException;
@@ -23,9 +24,7 @@ import net.cyklotron.cms.confirmation.EmailConfirmationRequestResource;
 import net.cyklotron.cms.confirmation.EmailConfirmationRequestService;
 import net.cyklotron.cms.modules.views.BaseSkinableScreen;
 import net.cyklotron.cms.poll.PollException;
-import net.cyklotron.cms.poll.PollResource;
 import net.cyklotron.cms.poll.PollService;
-import net.cyklotron.cms.poll.PollsResource;
 import net.cyklotron.cms.poll.VoteResource;
 import net.cyklotron.cms.preferences.PreferencesService;
 import net.cyklotron.cms.skins.SkinService;
@@ -61,8 +60,6 @@ public class Voting
         {
             Parameters parameters = RequestParameters.getRequestParameters(context);
             CoralSession coralSession = (CoralSession)context.getAttribute(CoralSession.class);
-            HttpContext httpContext = HttpContext.getHttpContext(context);
-            I18nContext i18nContext = I18nContext.getI18nContext(context);
             TemplatingContext templatingContext = TemplatingContext.getTemplatingContext(context);
             
             CmsData cmsData = cmsDataFactory.getCmsData(context);
@@ -115,7 +112,7 @@ public class Voting
         }
         catch(PollException e)
         {
-            throw new ProcessingException(e);
+            throw new ProcessingException("Vote not found." + e);
         }
     }
     
@@ -124,7 +121,6 @@ public class Voting
     {
         CoralSession coralSession = (CoralSession)context.getAttribute(CoralSession.class);
         HttpContext httpContext = HttpContext.getHttpContext(context);
-        I18nContext i18nContext = I18nContext.getI18nContext(context);
         TemplatingContext templatingContext = TemplatingContext.getTemplatingContext(context);
         CmsData cmsData = cmsDataFactory.getCmsData(context);
         
@@ -160,7 +156,19 @@ public class Voting
     public void prepareDefault(Context context)
     throws ProcessingException
     {
-
+        try
+        {
+            CoralSession coralSession = (CoralSession)context.getAttribute(CoralSession.class);
+            TemplatingContext templatingContext = TemplatingContext.getTemplatingContext(context);
+            CmsData cmsData = cmsDataFactory.getCmsData(context);
+            Parameters screenConfig = cmsData.getEmbeddedScreenConfig();
+            VoteResource vote = pollService.getVote(coralSession, screenConfig);
+            templatingContext.put("vote", vote);
+        }
+        catch(PollException e)
+        {
+            throw new ProcessingException("Vote not found." + e);
+        }
     }
     
     public void prepareConfirm(Context context)
@@ -170,8 +178,37 @@ public class Voting
     }
 
     public void prepareResult(Context context)
+    throws ProcessingException
     {
+        try
+        {
+            CoralSession coralSession = (CoralSession)context.getAttribute(CoralSession.class);
+            TemplatingContext templatingContext = TemplatingContext.getTemplatingContext(context);
+            CmsData cmsData = cmsDataFactory.getCmsData(context);
+            Parameters screenConfig = cmsData.getEmbeddedScreenConfig();
+            VoteResource vote = pollService.getVote(coralSession, screenConfig);
+            templatingContext.put("vote", vote);
 
+            Map answers = new HashMap();
+            Map resultMap= new HashMap();
+            Map percentMap= new HashMap();
+            Map ballotsMap= new HashMap();
+            
+            pollService.prepareVoteMaps(coralSession, vote, answers, resultMap, percentMap, ballotsMap);
+            List answerKeys = new ArrayList();
+            for(int i = 0; i< answers.size(); i++)
+            {
+                answerKeys.add(new Integer(i));
+            }
+            templatingContext.put("answerKeys", answerKeys);
+            templatingContext.put("results", resultMap);
+            templatingContext.put("percent", percentMap);
+            templatingContext.put("answers", answers);
+        }
+        catch(PollException e)
+        {
+            throw new ProcessingException("Vote not found." + e);
+        }
     }
     
     public void prepareTicketSent(Context context)
