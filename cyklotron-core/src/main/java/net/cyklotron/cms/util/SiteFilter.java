61c69ca0fd7a18d4e8ebd10973c82f195fe1cb8c
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2011-07-29 14:21:55 +0000

    CYKLO-831: generics

--- cyklotron-core/src/main/java/net/cyklotron/cms/util/SiteFilter.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/util/SiteFilter.java
@@ -1,19 +1,17 @@
 package net.cyklotron.cms.util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
-import org.objectledge.coral.session.CoralSession;
-import org.objectledge.coral.store.Resource;
-import org.objectledge.table.TableFilter;
 
 import net.cyklotron.cms.CmsTool;
 import net.cyklotron.cms.site.SiteException;
 import net.cyklotron.cms.site.SiteResource;
 import net.cyklotron.cms.site.SiteService;
+
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.store.Resource;
+import org.objectledge.table.TableFilter;
 
 /**
  * This is a filter for filtering resources upon their owner site,
@@ -22,35 +20,28 @@ import net.cyklotron.cms.site.SiteService;
  * @version $Id: SiteFilter.java,v 1.3 2005-02-09 22:20:08 rafal Exp $
  */
 public class SiteFilter
-    implements TableFilter
+    implements TableFilter<Resource>
 {
-    private Set acceptedSites;
+    private Set<SiteResource> acceptedSites;
 
     public SiteFilter(SiteResource[] acceptedSites)
     {
-        this.acceptedSites = new HashSet(Arrays.asList(acceptedSites));
+        this.acceptedSites = new HashSet<SiteResource>(Arrays.asList(acceptedSites));
     }
     
     public SiteFilter(CoralSession coralSession, String[] siteNames, SiteService siteService)
         throws SiteException
     {
-        List sites = new ArrayList();
+        acceptedSites = new HashSet<SiteResource>(siteNames.length);
         for(int i=0; i<siteNames.length; i++)
         {
-            sites.add(siteService.getSite(coralSession, siteNames[i]));
+            acceptedSites.add(siteService.getSite(coralSession, siteNames[i]));
         }
-        acceptedSites = new HashSet(sites);
     }
 
-    public boolean accept(Object object)
+    public boolean accept(Resource object)
     {
-        if(!(object instanceof Resource))
-        {
-            return false;
-        }
-
-        SiteResource site = CmsTool.getSite((Resource)object);
-
+        SiteResource site = CmsTool.getSite(object);
         return (site != null) && acceptedSites.contains(site);
     }
     
