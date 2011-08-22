9813dc5e15c543b4b25db33bd93b5936eaeea51f
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2011-08-22 13:41:35 +0000

    release 2.19-b1

---
+++ cyklotron-ui-fckeditor/src/main/webapp/content/ckeditor/plugins/cykloaddextresource/lang/pl.js
@@ -0,0 +1,9 @@
+﻿CKEDITOR.plugins.setLang("cykloaddextresource","pl",{
+  cykloaddextresource:
+    {
+	button_title:'Dołącz zasóbu zewnętrzy w wyglądzie NGO.pl',
+	dialog_title:'Dołącz zasóbu zewnętrzy w wyglądzie NGO.pl',
+	dialog_input_label:'Podaj URL zasobu (youtube, vimeo):',
+	dialog_input_validation:'Podano nieprawidłowy adresu URL'
+    }
+});
