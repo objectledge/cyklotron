426c2a5fdc04fed47d1bcf25469ac64da6a7fdbb
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2012-02-20 17:18:02 +0000

    release 2.20.7

---
+++ cyklotron-ui-fckeditor/src/main/webapp/content/ckeditor/_source/plugins/cyklopagebreak/lang/en.js
@@ -0,0 +1,8 @@
+﻿CKEDITOR.plugins.setLang("cyklopagebreak","en",{
+  cyklopagebreak:
+    {
+	title:'Insert cyklotron page brake',
+	commonTab:'Info',
+	HelpInfo:'Insert cyklotron page brake tag allows split content to pages.'
+    }
+});
