453b4383ed209c7e0596c8c92d8fb2652c8a1257
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2012-05-18 23:49:47 +0000

    release 2.20.14

---
+++ cyklotron-ui-fckeditor/src/main/webapp/content/ckeditor/_source/plugins/cyklopagebreak/lang/pl.js
@@ -0,0 +1,8 @@
+﻿CKEDITOR.plugins.setLang("cyklopagebreak","pl",{
+  cyklopagebreak:
+    {
+         title:'Wstaw cyklotronowy podział strony',
+	 commonTab:'Info',
+	 HelpInfo:'Wstawia cyklotronowy znacznik podziału strony. Dzieli treści na strony.'
+    }
+});
