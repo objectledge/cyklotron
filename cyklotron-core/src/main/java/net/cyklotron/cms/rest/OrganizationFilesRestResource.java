895b22cbd92abbcb5c1d004e95a94f19e07b5d6a
Marta Kalamar <marta.kalamar@caltha.pl>
2012-08-17 20:59:37 +0200

    organization files REST resource code scheleton.

--- cyklotron-core/src/main/java/net/cyklotron/cms/rest/OrganizationFilesRestResource.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/rest/OrganizationFilesRestResource.java
@@ -3,48 +3,43 @@ package net.cyklotron.cms.rest;
 import java.io.InputStream;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 
-import com.sun.jersey.multipart.FormDataParam;
 import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 
 @Produces({"application/xml","application/json"}) 
 @Path("/pliki")
 public class OrganizationFilesRestResource {
-
-	@Context UriInfo ui; 
 	
     @GET
     @Produces("application/json")
     @Path("/{orgId: [a-zA-Z0-9_]+}/{ftype}/{fname}")
-    public String getFile(@PathParam("{orgId}") String orgId, 
-    		@PathParam("{ftype}") String ftype,
-    		@PathParam("{fname}") String fname) {
+    public String getFile(@PathParam("orgId") String orgId, 
+    		@PathParam("ftype") String ftype,
+    		@PathParam("fname") String fname) {
         return "Pobierz pjedynczy plik :" + orgId + " ftype:" + ftype +" fname:" + fname;    
     }
 
     @GET
     @Path("/{orgId: [a-zA-Z0-9_]+}")
     @Produces("application/json")
-    public String getFiles(@PathParam("{orgId}") String orgId) {
-        return "Wszystkie pliki organizacji orgId:" + orgId + ", z podziałem an kategorie." + ui.getRequestUri().toString() ;    
+    public String getFiles(@PathParam("orgId") String orgId) {
+        return "Wszystkie pliki organizacji orgId:" + orgId + ", z podziałem an kategorie.";    
     }
 
     @GET
     @Path("/{orgId: [a-zA-Z0-9_]+}/{ftype}")
     @Produces("application/json")
-    public String getFiles(@PathParam("{orgId}") String orgId, 
-    		@PathParam("{ftype}") String ftype) {
+    public String getFiles(@PathParam("orgId") String orgId, 
+    		@PathParam("ftype") String ftype) {
         return "Wszystkie pliki organizacji orgId:" + orgId + ", typu:" + ftype + ".";    
     }
 
@@ -52,9 +47,9 @@ public class OrganizationFilesRestResource {
     @Produces("application/json")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Path("/{orgId: [a-zA-Z0-9_]+}/{ftype}/{fname}")
-    public String createFile(@PathParam("{orgId}") String orgId, 
-    		@PathParam("{ftype}") String ftype,
-    		@PathParam("{fname}") String fname,
+    public String createFile(@PathParam("orgId") String orgId, 
+    		@PathParam("ftype") String ftype,
+    		@PathParam("fname") String fname,
     		@FormDataParam("file") InputStream uploadedInputStream,
     		@FormDataParam("file") FormDataContentDisposition fileDetail
     		) {
@@ -66,9 +61,9 @@ public class OrganizationFilesRestResource {
     @Produces("application/json")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Path("/{orgId}/{ftype}/{fname}")
-    public String modifyFile(@PathParam("{orgId}") String orgId, 
-    		@PathParam("{ftype}") String ftype,
-    		@PathParam("{fname}") String fname,
+    public String modifyFile(@PathParam("orgId") String orgId, 
+    		@PathParam("ftype") String ftype,
+    		@PathParam("fname") String fname,
     		@FormDataParam("file") InputStream uploadedInputStream,
     		@FormDataParam("file") FormDataContentDisposition fileDetail) {
         return "Plik do modyfikacji\n orgId:" + orgId + " ftype:" + ftype +" fname:" + fname +
