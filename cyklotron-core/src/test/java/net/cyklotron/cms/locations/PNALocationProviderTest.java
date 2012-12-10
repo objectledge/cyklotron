3f7d4e06f782be292ef3a29102387a6501049f5e
Rafał Krzewski <rafal.krzewski@caltha.pl>
2012-12-10 20:43:18 +0100

    CYKLO-917: ensure test stability

--- cyklotron-core/src/test/java/net/cyklotron/cms/locations/PNALocationProviderTest.java
+++ cyklotron-core/src/test/java/net/cyklotron/cms/locations/PNALocationProviderTest.java
@@ -1,6 +1,8 @@
 package net.cyklotron.cms.locations;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.Collection;
 
 import org.apache.log4j.Level;
@@ -42,6 +44,15 @@ public class PNALocationProviderTest
         {
             initLog4J("ERROR");
             LogManager.getLogger(getClass()).setLevel(Level.INFO);
+            // JUnit can execute tests in arbitrary order. When db is empty and testFromCache runs
+            // first, load the data twice rather than fail
+            try(Connection conn = database.getConnection(); Statement stmt = conn.createStatement())
+            {
+                if(count(stmt, "locations_pna") < 100000)
+                {
+                    provider.fromSource();
+                }
+            }
             final Collection<Location> locations = provider.fromCache();
             assertTrue(locations.size() > 100000);
         }
