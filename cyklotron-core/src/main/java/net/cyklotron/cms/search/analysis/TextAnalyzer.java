8aeffabc1c0a09e7e9c9f7f270d244a4e7a80d24
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-01-17 16:59:05 +0100

    CYKLO-927 Rewrite TextAnalyzer. Copy StandardAnalyzer and make it work with TextTokenizer. Add Stemmer support just like in old TextAnalyzer. Null if not used.

--- cyklotron-core/src/main/java/net/cyklotron/cms/search/analysis/TextAnalyzer.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/search/analysis/TextAnalyzer.java
@@ -1,6 +1,6 @@
 package net.cyklotron.cms.search.analysis;
 
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -17,155 +17,162 @@ package net.cyklotron.cms.search.analysis;
  * limitations under the License.
  */
 
-import org.apache.lucene.analysis.*;
-import org.apache.lucene.analysis.standard.StandardFilter;
-import org.apache.lucene.util.Version;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Set;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.standard.ClassicAnalyzer;
+import org.apache.lucene.analysis.standard.ClassicTokenizer;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
+import org.apache.lucene.util.Version;
 
 /**
- * Filters {@link StandardTokenizer} with {@link StandardFilter}, {@link
- * LowerCaseFilter} and {@link StopFilter}, using a list of
- * English stop words.
- *
- * <a name="version"/>
- * <p>You must specify the required {@link Version}
- * compatibility when creating StandardAnalyzer:
+ * Filters {@link TextTokenizer} with {@link StandardFilter}, {@link LowerCaseFilter} and
+ * {@link StopFilter}, using a list of English stop words. <a name="version"/>
+ * <p>
+ * You must specify the required {@link Version} compatibility when creating TextAnalyzer:
  * <ul>
- *   <li> As of 2.9, StopFilter preserves position
- *        increments
- *   <li> As of 2.4, Tokens incorrectly identified as acronyms
- *        are corrected (see <a href="https://issues.apache.org/jira/browse/LUCENE-1068">LUCENE-1608</a>
+ * <li>As of 3.4, Hiragana and Han characters are no longer wrongly split from their combining
+ * characters. If you use a previous version number, you get the exact broken behavior for backwards
+ * compatibility.
+ * <li>As of 3.1, TextTokenizer implements Unicode text segmentation, and StopFilter correctly
+ * handles Unicode 4.0 supplementary characters in stopwords. {@link ClassicTokenizer} and
+ * {@link ClassicAnalyzer} are the pre-3.1 implementations of TextTokenizer and TextAnalyzer.
+ * <li>As of 2.9, StopFilter preserves position increments
+ * <li>As of 2.4, Tokens incorrectly identified as acronyms are corrected (see <a
+ * href="https://issues.apache.org/jira/browse/LUCENE-1068">LUCENE-1068</a>)
  * </ul>
  */
-public class TextAnalyzer extends Analyzer {
-  private Set<?> stopSet;
+public final class TextAnalyzer
+    extends StopwordAnalyzerBase
+{
 
-  /**
-   * Specifies whether deprecated acronyms should be replaced with HOST type.
-   * See {@linkplain https://issues.apache.org/jira/browse/LUCENE-1068}
-   */
-  private final boolean replaceInvalidAcronym,enableStopPositionIncrements;
+    /** Default maximum allowed token length */
+    public static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
 
-  /** An unmodifiable set containing some common English words that are usually not
-  useful for searching. */
-  public static final Set<?> STOP_WORDS_SET = StopAnalyzer.ENGLISH_STOP_WORDS_SET; 
-  /** Stemmer instance. Null when not used. */
-  private final Stemmer stemmer;
-  private final Version matchVersion;
+    private int maxTokenLength = DEFAULT_MAX_TOKEN_LENGTH;
 
-  /** Builds an analyzer with the default stop words ({@link
-   * #STOP_WORDS_SET}) and no stemmer.
-   * @param matchVersion Lucene version to match See {@link
-   * <a href="#version">above</a>}
-   */
-  public TextAnalyzer(Version matchVersion) {
-    this(matchVersion, STOP_WORDS_SET, null);
-  }
+    /**
+     * An unmodifiable set containing some common English words that are usually not useful for
+     * searching.
+     */
+    public static final CharArraySet STOP_WORDS_SET = StopAnalyzer.ENGLISH_STOP_WORDS_SET;
 
-  /** Builds an analyzer with the given stop words.
-   * @param matchVersion Lucene version to match See {@link
-   * <a href="#version">above</a>}
-   * @param stopWords stop words
-   * @param stemmer a Stemmer instance. Null when not used. */
-  public TextAnalyzer(Version matchVersion, Set<?> stopWords, Stemmer stemmer) {
-    stopSet = stopWords;
-    setOverridesTokenStreamMethod(TextAnalyzer.class);
-    enableStopPositionIncrements = StopFilter.getEnablePositionIncrementsVersionDefault(matchVersion);
-    replaceInvalidAcronym = matchVersion.onOrAfter(Version.LUCENE_24);
-    this.matchVersion = matchVersion;
-    this.stemmer = stemmer;
-  }
+    private final Stemmer stemmer;
 
-  /** Builds an analyzer with the stop words from the given file.
-   * @see WordlistLoader#getWordSet(File)
-   * @param matchVersion Lucene version to match See {@link
-   * <a href="#version">above</a>}
-   * @param stopwords File to read stop words from    
-   * @param stemmer a Stemmer instance. Null when not used. */
-  public TextAnalyzer(Version matchVersion, File stopwords, Stemmer stemmer) throws IOException {
-    this(matchVersion, WordlistLoader.getWordSet(stopwords), stemmer);
-  }
-
-  /** Builds an analyzer with the stop words from the given reader.
-   * @see WordlistLoader#getWordSet(Reader)
-   * @param matchVersion Lucene version to match See {@link
-   * <a href="#version">above</a>}
-   * @param stopwords Reader to read stop words from
-   * @param stemmer a Stemmer instance. Null when not used. */ 
-  public TextAnalyzer(Version matchVersion, Reader stopwords, Stemmer stemmer) throws IOException {
-    this(matchVersion, WordlistLoader.getWordSet(stopwords), stemmer);
-  }
-
-  /** Constructs a {@link StandardTokenizer} filtered by a {@link
-  StandardFilter}, a {@link LowerCaseFilter} and a {@link StopFilter}. */
-  @Override
-  public TokenStream tokenStream(String fieldName, Reader reader) {
-    TextTokenizer tokenStream = new TextTokenizer(matchVersion, reader);
-    tokenStream.setMaxTokenLength(maxTokenLength);
-    TokenStream result = new StandardFilter(tokenStream);
-    result = new LowerCaseFilter(result);
-    result = new StopFilter(enableStopPositionIncrements, result, stopSet);
-    return result;
-  }
-
-  private static final class SavedStreams {
-      TextTokenizer tokenStream;
-    TokenStream filteredTokenStream;
-  }
-
-  /** Default maximum allowed token length */
-  public static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
-
-  private int maxTokenLength = DEFAULT_MAX_TOKEN_LENGTH;
-
-  /**
-   * Set maximum allowed token length.  If a token is seen
-   * that exceeds this length then it is discarded.  This
-   * setting only takes effect the next time tokenStream or
-   * reusableTokenStream is called.
-   */
-  public void setMaxTokenLength(int length) {
-    maxTokenLength = length;
-  }
-    
-  /**
-   * @see #setMaxTokenLength
-   */
-  public int getMaxTokenLength() {
-    return maxTokenLength;
-  }
-
-  @Override
-  public TokenStream reusableTokenStream(String fieldName, Reader reader) throws IOException {
-    if (overridesTokenStreamMethod) {
-      // LUCENE-1678: force fallback to tokenStream() if we
-      // have been subclassed and that subclass overrides
-      // tokenStream but not reusableTokenStream
-      return tokenStream(fieldName, reader);
+    /**
+     * Builds an analyzer with the given stop words.
+     * 
+     * @param matchVersion Lucene version to match See {@link <a href="#version">above</a>}
+     * @param stopWords stop words
+     */
+    public TextAnalyzer(Version matchVersion, CharArraySet stopWords)
+    {
+        super(matchVersion, stopWords);
+        this.stemmer = null;
     }
-    SavedStreams streams = (SavedStreams) getPreviousTokenStream();
-    if (streams == null) {
-      streams = new SavedStreams();
-      setPreviousTokenStream(streams);
-      streams.tokenStream = new TextTokenizer(matchVersion, reader);
-      streams.filteredTokenStream = new StandardFilter(streams.tokenStream);
-      streams.filteredTokenStream = new LowerCaseFilter(streams.filteredTokenStream);
-      streams.filteredTokenStream = new StopFilter(enableStopPositionIncrements,
-                                                   streams.filteredTokenStream, stopSet);
-      if(stemmer != null) {
-          streams.filteredTokenStream = new StemFilter(streams.filteredTokenStream, stemmer);
-      }
-    } else {
-      streams.tokenStream.reset(reader);
-    }
-    streams.tokenStream.setMaxTokenLength(maxTokenLength);
-    
-    streams.tokenStream.setReplaceInvalidAcronym(replaceInvalidAcronym);
 
-    return streams.filteredTokenStream;
-  }
+    /**
+     * Builds an analyzer with the given stop words.
+     * 
+     * @param matchVersion Lucene version to match See {@link <a href="#version">above</a>}
+     * @param stopWords stop words
+     */
+    public TextAnalyzer(Version matchVersion, CharArraySet stopWords, Stemmer stemmer)
+    {
+        super(matchVersion, stopWords);
+        this.stemmer = stemmer;
+    }
+
+    /**
+     * Builds an analyzer with the default stop words ({@link #STOP_WORDS_SET}).
+     * 
+     * @param matchVersion Lucene version to match See {@link <a href="#version">above</a>}
+     */
+    public TextAnalyzer(Version matchVersion)
+    {
+        this(matchVersion, STOP_WORDS_SET);
+    }
+
+    /**
+     * Builds an analyzer with the default stop words ({@link #STOP_WORDS_SET}).
+     * 
+     * @param matchVersion Lucene version to match See {@link <a href="#version">above</a>}
+     */
+    public TextAnalyzer(Version matchVersion, Stemmer stemmer)
+    {
+        this(matchVersion, STOP_WORDS_SET);
+    }
+
+    /**
+     * Builds an analyzer with the stop words from the given reader.
+     * 
+     * @see WordlistLoader#getWordSet(Reader, Version)
+     * @param matchVersion Lucene version to match See {@link <a href="#version">above</a>}
+     * @param stopwords Reader to read stop words from
+     */
+    public TextAnalyzer(Version matchVersion, Reader stopwords)
+        throws IOException
+    {
+        this(matchVersion, loadStopwordSet(stopwords, matchVersion));
+    }
+
+    /**
+     * Builds an analyzer with the stop words from the given reader.
+     * 
+     * @see WordlistLoader#getWordSet(Reader, Version)
+     * @param matchVersion Lucene version to match See {@link <a href="#version">above</a>}
+     * @param stopwords Reader to read stop words from
+     */
+    public TextAnalyzer(Version matchVersion, Reader stopwords, Stemmer stemmer)
+        throws IOException
+    {
+        this(matchVersion, loadStopwordSet(stopwords, matchVersion), stemmer);
+    }
+
+    /**
+     * Set maximum allowed token length. If a token is seen that exceeds this length then it is
+     * discarded. This setting only takes effect the next time tokenStream or tokenStream is called.
+     */
+    public void setMaxTokenLength(int length)
+    {
+        maxTokenLength = length;
+    }
+
+    /**
+     * @see #setMaxTokenLength
+     */
+    public int getMaxTokenLength()
+    {
+        return maxTokenLength;
+    }
+
+    @Override
+    protected TokenStreamComponents createComponents(final String fieldName, final Reader reader)
+    {
+        final TextTokenizer src = new TextTokenizer(matchVersion, reader);
+        src.setMaxTokenLength(maxTokenLength);
+        TokenStream tok = new StandardFilter(matchVersion, src);
+        tok = new LowerCaseFilter(matchVersion, tok);
+        tok = new StopFilter(matchVersion, tok, stopwords);
+        if(stemmer != null)
+        {
+            tok = new StemFilter(tok, stemmer);
+        }
+        return new TokenStreamComponents(src, tok)
+            {
+                @Override
+                protected void setReader(final Reader reader)
+                    throws IOException
+                {
+                    src.setMaxTokenLength(TextAnalyzer.this.maxTokenLength);
+                    super.setReader(reader);
+                }
+            };
+    }
 }
