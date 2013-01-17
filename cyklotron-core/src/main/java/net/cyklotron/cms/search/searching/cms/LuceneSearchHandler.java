3d57e150a8b9b861ec8e429f7cb649636e0d383b
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-01-17 18:59:33 +0100

    CYKLO-927 Reformat code.

--- cyklotron-core/src/main/java/net/cyklotron/cms/search/searching/cms/LuceneSearchHandler.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/search/searching/cms/LuceneSearchHandler.java
@@ -48,15 +48,17 @@ import com.google.common.base.Optional;
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
  * @version $Id: LuceneSearchHandler.java,v 1.8 2013-01-17 07:29:35 marek Exp $
  */
-public class LuceneSearchHandler implements SearchHandler<LuceneSearchHit>
+public class LuceneSearchHandler
+    implements SearchHandler<LuceneSearchHit>
 {
     /** search service for getting searchers. */
     private SearchService searchService;
+
     /** integration service for building URLs for search hits. */
     private IntegrationService integrationService;
 
     private Context context;
-    
+
     public LuceneSearchHandler(Context context, SearchService searchService,
         IntegrationService integrationService, CmsDataFactory cmsDateFactory)
     {
@@ -87,11 +89,11 @@ public class LuceneSearchHandler implements SearchHandler<LuceneSearchHit>
         {
             sort = new Sort(sortFields);
         }
-        
+
         // get index pools from chosen search pools
         PoolResource[] pools = null;
         ArrayList<PoolResource> tmpPools = new ArrayList<PoolResource>(searchPools.length);
-        for(int i=0; i<searchPools.length; i++)
+        for(int i = 0; i < searchPools.length; i++)
         {
             Resource pool = searchPools[i];
             if(pool instanceof PoolResource)
@@ -101,13 +103,13 @@ public class LuceneSearchHandler implements SearchHandler<LuceneSearchHit>
         }
         pools = new PoolResource[tmpPools.size()];
         pools = (PoolResource[])(tmpPools.toArray(pools));
-        
+
         // search
         IndexSearcher searcher = null;
         List<LuceneSearchHit> hits;
         try
-        {	
-        	Timer timer = new Timer();
+        {
+            Timer timer = new Timer();
             Optional<IndexSearcher> optional = searchService.getSearchingFacility().getSearcher(
                 pools, coralSession.getUserSubject());
             if(optional.isPresent())
@@ -146,14 +148,15 @@ public class LuceneSearchHandler implements SearchHandler<LuceneSearchHit>
         CoralSession coralSession)
     {
         // prepare link tool
-        TemplatingContext tContext = (TemplatingContext)
-        context.getAttribute(TemplatingContext.class);
+        TemplatingContext tContext = (TemplatingContext)context
+            .getAttribute(TemplatingContext.class);
         CmsLinkTool link = (CmsLinkTool)tContext.get("link");
         link = (CmsLinkTool)(link.unsetAction().unsetView());
-        
+
         Subject subject = coralSession.getUserSubject();
         AuthenticationContext authContext = AuthenticationContext.getAuthenticationContext(context);
-        TableModel<LuceneSearchHit> model = new HitsTableModel<LuceneSearchHit>(context, hits, this, link, subject, authContext.isUserAuthenticated());
+        TableModel<LuceneSearchHit> model = new HitsTableModel<LuceneSearchHit>(context, hits,
+            this, link, subject, authContext.isUserAuthenticated());
         return model;
     }
 
@@ -176,7 +179,7 @@ public class LuceneSearchHandler implements SearchHandler<LuceneSearchHit>
             hits = topFieldCollector.topDocs();
         }
         ScoreDoc[] scoreDocs = hits.scoreDocs;
-        float percent = 1/hits.getMaxScore();
+        float percent = 1 / hits.getMaxScore();
         Set<LuceneSearchHit> searchHits = new LinkedHashSet<LuceneSearchHit>(scoreDocs.length);
         for(int i = 0; i < scoreDocs.length; i++)
         {
@@ -188,12 +191,12 @@ public class LuceneSearchHandler implements SearchHandler<LuceneSearchHit>
     }
 
     ResourceClassResource getHitResourceClassResource(CoralSession coralSession, LuceneSearchHit hit)
-    throws EntityDoesNotExistException
+        throws EntityDoesNotExistException
     {
-        return integrationService.getResourceClass(coralSession,
-            coralSession.getSchema().getResourceClass(hit.getResourceClassId()));
+        return integrationService.getResourceClass(coralSession, coralSession.getSchema()
+            .getResourceClass(hit.getResourceClassId()));
     }
-    
+
     Resource getHitResource(CoralSession coralSession, LuceneSearchHit hit)
         throws EntityDoesNotExistException
     {
@@ -206,7 +209,7 @@ public class LuceneSearchHandler implements SearchHandler<LuceneSearchHit>
             return null;
         }
     }
-    
+
     public void resolveUrls(LuceneSearchHit hit, Subject subject, Context context,
         boolean generateEditLink, LinkTool link)
     {
