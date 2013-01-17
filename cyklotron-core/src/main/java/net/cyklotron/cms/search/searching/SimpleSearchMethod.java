72284b2a5f243ecf6be784ca72b62777a3d45f18
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-01-17 17:19:54 +0100

    CYKLO-927 Fix package changes.

--- cyklotron-core/src/main/java/net/cyklotron/cms/search/searching/SimpleSearchMethod.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/search/searching/SimpleSearchMethod.java
@@ -4,8 +4,8 @@ import java.util.Locale;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.DateTools;
-import org.apache.lucene.queryParser.MultiFieldQueryParser;
-import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.util.Version;
 import org.objectledge.coral.session.CoralSession;
@@ -40,7 +40,7 @@ public class SimpleSearchMethod extends BaseSearchMethod
         if(query.length() > 0)
         {
             Analyzer analyzer = searchService.getAnalyzer(locale);
-            QueryParser parser = new MultiFieldQueryParser(Version.LUCENE_30, DEFAULT_FIELD_NAMES,
+            QueryParser parser = new MultiFieldQueryParser(Version.LUCENE_40, DEFAULT_FIELD_NAMES,
                 analyzer);
             parser.setDateResolution(DateTools.Resolution.SECOND);
             return parser.parse(query);
