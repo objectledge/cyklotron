4679a6f807e46a5c33a305624fd134cd9045ad5d
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-01-22 00:28:10 +0100

    CYKLO-927 Add test which adds document to index and then validates that stems and original words are available.

--- cyklotron-core/src/test/java/net/cyklotron/cms/search/analysis/StemFilterTest.java
+++ cyklotron-core/src/test/java/net/cyklotron/cms/search/analysis/StemFilterTest.java
@@ -2,27 +2,46 @@ package net.cyklotron.cms.search.analysis;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.MultiFields;
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.Version;
+import org.fest.assertions.Fail;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import net.cyklotron.cms.search.SearchConstants;
 
 public class StemFilterTest
 {
     // source of words: news @ wp.pl
     // text has been down cased by hand
-    private final static String textToStem = "to uczyniło go prezydentem usa zdjęcia"
+    private final static String ORIGINAL_TEXT = "to uczyniło go prezydentem usa zdjęcia"
         + "poród i śmierć na plebanii arcybiskup pisze do papieża"
         + "politycy mogą mieć wyborców w nosie ale do czasu"
         + "decyzja zapadła rosja wysyła samoloty w rejon wojny"
@@ -40,7 +59,7 @@ public class StemFilterTest
     @BeforeMethod
     public void generateWordsAndStems()
     {
-        String[] words = textToStem.split(" ");
+        String[] words = ORIGINAL_TEXT.split(" ");
         Stemmer stemmer = new StemmerPL();
         originalWords = Arrays.asList(words);
         expectedStreamOfWords = new ArrayList<>();
@@ -64,7 +83,7 @@ public class StemFilterTest
 
         TextAnalyzer analyzer = new TextAnalyzer(Version.LUCENE_40, stopWordsReader,
             new StemmerPL());
-        TokenStream stream = analyzer.tokenStream("field", new StringReader(textToStem));
+        TokenStream stream = analyzer.tokenStream("field", new StringReader(ORIGINAL_TEXT));
 
         // get the CharTermAttribute from the TokenStream
         CharTermAttribute termAtt = stream.addAttribute(CharTermAttribute.class);
@@ -91,4 +110,110 @@ public class StemFilterTest
         assertThat(stemmedResults).isEqualTo(expectedStreamOfWords);
     }
 
+    @Test(dataProvider = "getDocuments")
+    public void should_not_fail_when_writing_into_index(Document document,
+        Map<String, Collection<String>> fieldToTerms)
+        throws Exception
+    {
+        // given
+        RAMDirectory directory = new RAMDirectory();
+        String stopwords = "";
+        final StringReader stopWordsReader = new StringReader(stopwords);
+
+        Analyzer analyzer = createPerFieldAnalyzer();
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+        IndexWriter writer = new IndexWriter(directory, indexWriterConfig);
+        
+        // when
+        try
+        {
+            writer.addDocument(document);
+        }
+        catch(IOException ex)
+        {
+            Fail.fail("should not throw errors", ex);
+        }
+        finally
+        {
+            writer.close();
+        }
+        // then
+        DirectoryReader reader = DirectoryReader.open(directory);
+        Fields fields = MultiFields.getFields(reader);
+        Iterator<String> iterator = fields.iterator();
+        String field;
+        while(iterator.hasNext())
+        {
+            field = iterator.next();
+            Terms terms = fields.terms(field);
+            TermsEnum termsEnum = terms.iterator(null);
+            BytesRef term;
+            while((term = termsEnum.next()) != null)
+            {
+                String termStr = term.utf8ToString();
+                assertThat(termStr).isIn(fieldToTerms.get(field));
+            }
+        }
+    }
+
+
+    private Analyzer createPerFieldAnalyzer()
+        throws IOException
+    {
+
+        Map<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
+        analyzerPerField.put(SearchConstants.FIELD_CATEGORY, new NewlineSeparatedAnalyzer());
+        Stemmer stemmer = new StemmerPL();
+        analyzerPerField.put(SearchConstants.FIELD_INDEX_ABBREVIATION, new TextAnalyzer(
+            Version.LUCENE_40, getStopwordsReader(), stemmer));
+        analyzerPerField.put(SearchConstants.FIELD_INDEX_TITLE, new TextAnalyzer(Version.LUCENE_40,
+            getStopwordsReader(), stemmer));
+        analyzerPerField.put(SearchConstants.FIELD_INDEX_CONTENT, new TextAnalyzer(
+            Version.LUCENE_40, getStopwordsReader(), stemmer));
+        return new PerFieldAnalyzerWrapper(new TextAnalyzer(Version.LUCENE_40), analyzerPerField);
+    }
+
+    private Reader getStopwordsReader()
+    {
+        return new StringReader("");
+    }
+
+    @DataProvider
+    private Object[][] getDocuments()
+    {
+        Document dt1 = new Document();
+        Map<String, Collection<String>> dt1fieldToTerms = new HashMap<>();
+        String dt1TitleStr = "przykładowy tytuł jakiegoś dokumentu";
+        Collection<String> dt1stemmedTitle = getStemmed(dt1TitleStr);
+        dt1fieldToTerms.put(SearchConstants.FIELD_INDEX_TITLE, dt1stemmedTitle);
+        TextField dt1Title = new TextField(SearchConstants.FIELD_INDEX_TITLE, dt1TitleStr,
+            Store.YES);
+        String defaultText = "ja chcieć tralala kiełbasa mmm lubie to";
+        TextField dtDefault = new TextField("defaultFieldName",
+ defaultText, Store.NO);
+        Collection<String> dt1stemmedContent = getStemmed(ORIGINAL_TEXT);
+        dt1fieldToTerms.put(SearchConstants.FIELD_INDEX_CONTENT, dt1stemmedContent);
+        TextField dt1Content = new TextField(SearchConstants.FIELD_INDEX_CONTENT, ORIGINAL_TEXT,
+            Store.YES);
+        dt1.add(dt1Title);
+        dt1.add(dtDefault);
+        dt1.add(dt1Content);
+
+        dt1fieldToTerms.put("defaultFieldName", Arrays.asList(defaultText.split(" ")));
+
+        return new Object[][] { { dt1, dt1fieldToTerms } };
+    }
+
+    private Collection<String> getStemmed(String content)
+    {
+        Stemmer stemmer = new StemmerPL();
+        String[] words = content.split(" ");
+        Collection<String> stemmed = new ArrayList<>();
+        for(String word : words)
+        {
+            stemmed.add(word);
+            stemmed.add(stemmer.stem(word));
+        }
+        return stemmed;
+    }
 }
