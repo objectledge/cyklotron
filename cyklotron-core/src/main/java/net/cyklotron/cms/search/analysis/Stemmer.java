7eae1b979f536fd68ef344c350bf2818e47d4709
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-01-23 12:49:26 +0100

    CYKLO-927 Update javadoc.

--- cyklotron-core/src/main/java/net/cyklotron/cms/search/analysis/Stemmer.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/search/analysis/Stemmer.java
@@ -3,12 +3,12 @@ package net.cyklotron.cms.search.analysis;
 /**
  * A stemmer for a specific language.
  * 
- * @author rafal
+ * @author rafal, marek
  */
 public interface Stemmer
 {
     /**
-     * Returns a stem for a term.
+     * Returns a stem for a term or null if Stempel could not stem this term.
      * 
      * @param term the term.
      * @return the stem of the term. If stem could not be determined whole word should be retured.
