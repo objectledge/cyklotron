3102d800c371f3a2b94f13c65d226c8d939e6042
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-01-25 16:07:54 +0100

    CYKLO-927 Refactor AlphanumericFilter to use PatternReplaceFilter which probably has more optimal code.

--- cyklotron-core/src/main/java/net/cyklotron/cms/search/analysis/AlphanumericFilter.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/search/analysis/AlphanumericFilter.java
@@ -4,39 +4,39 @@
 package net.cyklotron.cms.search.analysis;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.pattern.PatternReplaceFilter;
 
 /**
- * A token filter that retains only characters that belong to Unicode L and N classes. 
- *  
- *  This filter is useful for stripping quotes, parens and punctuation from token stream. 
- *  
- * @author rafal
+ * A token filter that retains only characters that belong to Unicode L and N classes. This filter
+ * is useful for stripping quotes, parens and punctuation from token stream.
+ * 
+ * @author rafal, marek
  */
 public class AlphanumericFilter
     extends TokenFilter
 {
-    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+    private static final String NON_ALPHANUMERIC = "[^\\p{L}\\p{N}]";
+
+    private static final Pattern pattern = Pattern.compile(NON_ALPHANUMERIC);
+
+    private final PatternReplaceFilter patternReplaceFilter;
+
+    private static final boolean REPLACE_ALL = true;
 
     public AlphanumericFilter(TokenStream input)
     {
         super(input);
+        patternReplaceFilter = new PatternReplaceFilter(input, pattern, "", REPLACE_ALL);
     }
 
     @Override
     public boolean incrementToken()
         throws IOException
     {
-        if(!input.incrementToken())
-        {
-            return false;
-        }
-        final String replaced = termAtt.toString().replaceAll("[^\\p{L}\\p{N}]", "");
-        // TODO validate that this is correct. I still don't have a feeling about those buffers
-        termAtt.copyBuffer(replaced.toCharArray(), 0, replaced.length());
-        return true;
+        return patternReplaceFilter.incrementToken();
     }
 }
