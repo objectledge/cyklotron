a4c2f1d5d6469a170f04c4cacb32ff38d6aa86ff
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-06 14:32:53 +0100

    BAZYNGO-352 Add javadoc and throws.

--- cyklotron-core/src/main/java/net/cyklotron/cms/search/util/PerformSearch.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/search/util/PerformSearch.java
@@ -1,5 +1,6 @@
 package net.cyklotron.cms.search.util;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import org.apache.lucene.document.Document;
@@ -7,5 +8,14 @@ import org.apache.lucene.search.IndexSearcher;
 
 public interface PerformSearch
 {
-    Collection<Document> doSearch(IndexSearcher searcher);
+    /**
+     * Use it this method to do your query. Do not close searcher, just use it and let it go out of
+     * scope.
+     * 
+     * @param searcher IndexSearcher over index.
+     * @return collection of documents you want to get transformed into resources from your index
+     * @throws IOException
+     */
+    Collection<Document> doSearch(IndexSearcher searcher)
+        throws IOException;
 }
