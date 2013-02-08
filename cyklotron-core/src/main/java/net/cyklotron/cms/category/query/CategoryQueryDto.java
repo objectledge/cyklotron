7e5eeae7dce66b3933200f3edc8abf6567148c08
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-08 16:26:35 +0100

    BAZYNGO-666 Add CategoryQueryDto with context. Add more fields if needed.

---
+++ cyklotron-core/src/main/java/net/cyklotron/cms/category/query/CategoryQueryDto.java
@@ -0,0 +1,81 @@
+package net.cyklotron.cms.category.query;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class CategoryQueryDto
+{
+
+    private long id;
+
+    private String name;
+
+    private String query;
+
+    private String longQuery;
+
+    public static Collection<CategoryQueryDto> toDtos(
+        Collection<CategoryQueryResource> categoryQueries)
+    {
+        Collection<CategoryQueryDto> categoryQueriesDto = new ArrayList<>(categoryQueries.size());
+        for(CategoryQueryResource categoryQueryResource : categoryQueries)
+        {
+            categoryQueriesDto.add(toDto(categoryQueryResource));
+        }
+        return categoryQueriesDto;
+    }
+
+    private static CategoryQueryDto toDto(CategoryQueryResource categoryQueryResource)
+    {
+        CategoryQueryDto categoryQueryDto = new CategoryQueryDto();
+        categoryQueryDto.id = categoryQueryResource.getId();
+        categoryQueryDto.name = categoryQueryResource.getName();
+        categoryQueryDto.query = categoryQueryResource.getQuery();
+        categoryQueryDto.longQuery = categoryQueryResource.getLongQuery();
+        return categoryQueryDto;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getQuery()
+    {
+        return query;
+    }
+
+    public void setQuery(String query)
+    {
+        this.query = query;
+    }
+
+    public String getLongQuery()
+    {
+        return longQuery;
+    }
+
+    public void setLongQuery(String longQuery)
+    {
+        this.longQuery = longQuery;
+    }
+
+}
