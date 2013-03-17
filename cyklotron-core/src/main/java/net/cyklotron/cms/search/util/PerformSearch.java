27c496df8ba8642e2f5c3c6063c72a20d188f718
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-03-17 17:53:40 +0100

    BAZYNGO-782 Rename

--- cyklotron-core/src/main/java/net/cyklotron/cms/search/util/PerformSearch.java
+++ cyklotron-core/src/main/java/net/cyklotron/cms/search/util/PerformSearch.java
@@ -12,10 +12,10 @@ public interface PerformSearch
      * Use it this method to do your query. Do not close searcher, just use it and let it go out of
      * scope.
      * 
-     * @param searcher IndexSearcher over index.
+     * @param indexSearcher IndexSearcher over index.
      * @return collection of documents you want to get transformed into resources from your index
      * @throws IOException
      */
-    Collection<Document> doSearch(IndexSearcher searcher)
+    Collection<Document> doSearch(IndexSearcher indexSearcher)
         throws IOException;
 }
