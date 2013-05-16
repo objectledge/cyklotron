7c0caa22c0352d88b9f494bf94f81f20e4e06a35
Tomasz Matuszewski <tomasz.matuszewski@caltha.pl>
2013-05-16 11:48:30 +0200

    BAZYNGO-923
    Update checking account status

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/rest/login/AccountStatus.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/rest/login/AccountStatus.java
@@ -13,12 +13,14 @@ import javax.ws.rs.core.Response;
 import org.objectledge.authentication.AuthenticationException;
 import org.objectledge.authentication.BlockedReason;
 import org.objectledge.authentication.UserManager;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.parameters.directory.DirectoryParameters;
 
 @Path("/login")
 public class AccountStatus
 {
     private final UserManager userManager;
-    
+
     @Inject
     public AccountStatus(UserManager userManager)
     {
@@ -34,11 +36,19 @@ public class AccountStatus
             Principal account = userManager.getUserByLogin(uid);
             if(userManager.isUserPasswordExpired(account))
             {
-                userManager.setUserShadowFlag(account, BlockedReason.PASSWORD_EXPIRED.getCode().toString());
-            }        
+                userManager.setUserShadowFlag(account, BlockedReason.PASSWORD_EXPIRED.getCode()
+                    .toString());
+            }
+            Parameters params = new DirectoryParameters(userManager.getPersonalData(account));
+            String email = params.get("mail", "");
+            BlockedReason blockedReason = userManager.checkAccountFlag(account);
+            if(userManager.isEmailDuplicated(email))
+            {
+                blockedReason = BlockedReason.ACCOUNT_EMAIL_DUPLICATED;
+            }
             Long expiration = userManager.getUserPasswordExpirationDays(account);
-            BlockedReason blockedReason = userManager.checkAccountFlag(account);       
-            AccountStatusDto result = new AccountStatusDto(uid, blockedReason.getShortReason(), expiration);
+            AccountStatusDto result = new AccountStatusDto(uid, blockedReason.getShortReason(),
+                expiration);
             return Response.ok(result).build();
         }
         catch(AuthenticationException e)
@@ -48,4 +58,3 @@ public class AccountStatus
         }
     }
 }
-
