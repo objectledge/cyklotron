1540190823eb4cd33834b89ec9bf8eac1b9432e0
Rafał Krzewski <rafal.krzewski@caltha.pl>
2013-10-01 14:51:13 +0200

    BAZYNGO-1327: system wide facility for defining preferred image sizes.

---
+++ cyklotron-core/src/main/java/net/cyklotron/cms/documents/PreferredImageSizes.java
@@ -0,0 +1,16 @@
+package net.cyklotron.cms.documents;
+
+public class PreferredImageSizes
+{
+    private final int large;
+
+    public PreferredImageSizes(int large)
+    {
+        this.large = large;
+    }
+
+    public int getLarge()
+    {
+        return large;
+    }
+}
