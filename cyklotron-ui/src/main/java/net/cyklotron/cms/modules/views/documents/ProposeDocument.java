58b44e572bd16c839be08c7af30b743362449220
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-01-20 11:01:26 +0100

    BAZYNGO-1427: fitering on MyDocuments screen based on category queries

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/documents/ProposeDocument.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/documents/ProposeDocument.java
@@ -9,6 +9,11 @@ import java.util.Set;
 import org.jcontainer.dna.Logger;
 import org.objectledge.authentication.AuthenticationContext;
 import org.objectledge.context.Context;
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.query.MalformedQueryException;
+import org.objectledge.coral.query.QueryResults;
+import org.objectledge.coral.relation.MalformedRelationQueryException;
+import org.objectledge.coral.relation.ResourceIdentifierResolver;
 import org.objectledge.coral.schema.ResourceClass;
 import org.objectledge.coral.security.Permission;
 import org.objectledge.coral.security.Subject;
@@ -21,7 +26,9 @@ import org.objectledge.parameters.Parameters;
 import org.objectledge.parameters.RequestParameters;
 import org.objectledge.pipeline.ProcessingException;
 import org.objectledge.table.InverseFilter;
+import org.objectledge.table.TableException;
 import org.objectledge.table.TableFilter;
+import org.objectledge.table.TableModel;
 import org.objectledge.table.TableState;
 import org.objectledge.table.TableStateManager;
 import org.objectledge.table.TableTool;
@@ -30,7 +37,9 @@ import org.objectledge.web.mvc.finders.MVCFinder;
 
 import net.cyklotron.cms.CmsData;
 import net.cyklotron.cms.CmsDataFactory;
+import net.cyklotron.cms.category.CategoryResource;
 import net.cyklotron.cms.category.CategoryService;
+import net.cyklotron.cms.category.query.CategoryQueryResource;
 import net.cyklotron.cms.documents.DocumentNodeResource;
 import net.cyklotron.cms.documents.DocumentNodeResourceImpl;
 import net.cyklotron.cms.documents.DocumentService;
@@ -38,7 +47,9 @@ import net.cyklotron.cms.organizations.Organization;
 import net.cyklotron.cms.organizations.OrganizationRegistryService;
 import net.cyklotron.cms.preferences.PreferencesService;
 import net.cyklotron.cms.related.RelatedService;
+import net.cyklotron.cms.site.SiteException;
 import net.cyklotron.cms.site.SiteResource;
+import net.cyklotron.cms.site.SiteService;
 import net.cyklotron.cms.skins.SkinService;
 import net.cyklotron.cms.structure.NavigationNodeResourceImpl;
 import net.cyklotron.cms.structure.StructureService;
@@ -48,7 +59,12 @@ import net.cyklotron.cms.style.StyleService;
 import net.cyklotron.cms.workflow.AutomatonResource;
 import net.cyklotron.cms.workflow.StateFilter;
 import net.cyklotron.cms.workflow.StateResource;
+import net.cyklotron.cms.workflow.StatefulResource;
 import net.cyklotron.cms.workflow.WorkflowService;
+
+import bak.pcj.LongIterator;
+import bak.pcj.set.LongOpenHashSet;
+import bak.pcj.set.LongSet;
 
 /**
  * Stateful screen for propose document application.
@@ -74,12 +90,15 @@ public class ProposeDocument
 
     private final DocumentService documentService;
 
+    private final SiteService siteService;
+
     public ProposeDocument(org.objectledge.context.Context context, Logger logger,
         PreferencesService preferencesService, CmsDataFactory cmsDataFactory,
         StructureService structureService, StyleService styleService, SkinService skinService,
         MVCFinder mvcFinder, TableStateManager tableStateManager, CategoryService categoryService,
         RelatedService relatedService, HTMLService htmlService, WorkflowService workflowService,
-        OrganizationRegistryService ngoDatabaseService, DocumentService documentService)
+        OrganizationRegistryService ngoDatabaseService, DocumentService documentService,
+        SiteService siteService)
     {
         super(context, logger, preferencesService, cmsDataFactory, structureService, styleService,
                         skinService, mvcFinder, tableStateManager);
@@ -89,6 +108,7 @@ public class ProposeDocument
         this.workflowService = workflowService;
         this.organizationsRegistry = ngoDatabaseService;
         this.documentService = documentService;
+        this.siteService = siteService;
     }
 
     @Override
@@ -246,26 +266,35 @@ public class ProposeDocument
             I18nContext i18nContext = context.getAttribute(I18nContext.class);
             TemplatingContext templatingContext = context.getAttribute(TemplatingContext.class);
             CmsData cmsData = cmsDataFactory.getCmsData(context);
+            Parameters screenConfig = cmsData.getEmbeddedScreenConfig();
             CoralSession coralSession = context.getAttribute(CoralSession.class);
-            String query = "FIND RESOURCE FROM documents.document_node WHERE created_by = "
-                + coralSession.getUserSubject().getIdString() + " AND site = "
-                + cmsData.getSite().getIdString();
-            List<Resource> myDocuments = (List<Resource>)coralSession.getQuery()
-                .executeQuery(query).getList(1);
+            CategoryQueryResource includeQuery = getQueryResource("include", screenConfig,
+                coralSession);
+            CategoryQueryResource excludeQuery = getQueryResource("exclude", screenConfig,
+                coralSession);
 
-            ResourceListTableModel<Resource> model = new ResourceListTableModel<Resource>(
-                myDocuments, i18nContext.getLocale());
+            TableModel<DocumentNodeResource> model;
 
-            List<TableFilter<Resource>> filters = new ArrayList<TableFilter<Resource>>();
-            ResourceClass navigationNodeClass = coralSession.getSchema().getResourceClass(
+            if(includeQuery == null)
+            {
+                model = siteBasedModel(cmsData, i18nContext, coralSession);
+            }
+            else
+            {
+                model = queryBasedModel(includeQuery, excludeQuery, cmsData, i18nContext,
+                    coralSession);
+            }
+
+            List<TableFilter<? super DocumentNodeResource>> filters = new ArrayList<>();
+            ResourceClass<?> navigationNodeClass = coralSession.getSchema().getResourceClass(
                 "structure.navigation_node");
             Resource cmsRoot = coralSession.getStore().getUniqueResourceByPath("/cms");
             AutomatonResource automaton = workflowService.getPrimaryAutomaton(coralSession,
                 cmsRoot, navigationNodeClass);
             Set<StateResource> rejectedStates = new HashSet<StateResource>();
             rejectedStates.add(workflowService.getState(coralSession, automaton, "expired"));
-
-            filters.add(new InverseFilter(new StateFilter(rejectedStates, true)));
+            filters.add(new InverseFilter<StatefulResource>(new StateFilter(rejectedStates,
+                true)));
 
             TableState state = tableStateManager.getState(context, this.getClass().getName()
                 + ":MyDocuments");
@@ -276,13 +305,166 @@ public class ProposeDocument
                 state.setAscSort(false);
                 state.setPageSize(20);
             }
-            templatingContext.put("table", new TableTool<Resource>(state, filters, model));
+            templatingContext.put("table", new TableTool<DocumentNodeResource>(state, filters,
+                model));
             templatingContext.put("documentState", new DocumentStateTool(coralSession, logger));
         }
         catch(Exception e)
         {
             throw new ProcessingException("internal errror", e);
         }
+    }
+
+    private TableModel<DocumentNodeResource> siteBasedModel(CmsData cmsData,
+        I18nContext i18nContext, CoralSession coralSession)
+        throws MalformedQueryException, TableException
+    {
+        String query = "FIND RESOURCE FROM documents.document_node WHERE created_by = "
+            + coralSession.getUserSubject().getIdString() + " AND site = "
+            + cmsData.getSite().getIdString();
+
+        List<DocumentNodeResource> myDocuments = (List<DocumentNodeResource>)coralSession
+            .getQuery().executeQuery(query).getList(1);
+
+        return new ResourceListTableModel<DocumentNodeResource>(myDocuments,
+            i18nContext.getLocale());
+    }
+
+    private TableModel<DocumentNodeResource> queryBasedModel(CategoryQueryResource includeQuery,
+        CategoryQueryResource excludeQuery, CmsData cmsData, I18nContext i18nContext,
+        CoralSession coralSession)
+        throws SiteException, MalformedQueryException, MalformedRelationQueryException,
+        EntityDoesNotExistException, TableException
+    {
+        String resQuery = "FIND RESOURCE FROM documents.document_node WHERE created_by = "
+            + coralSession.getUserSubject().getIdString();
+
+        QueryResults qr = coralSession.getQuery().executeQuery(resQuery);
+        LongSet ids = new LongOpenHashSet(qr.rowCount());
+        for(QueryResults.Row r : qr.getList())
+        {
+            ids.add(r.getId());
+        }
+
+        String catQuery = includeQuery.getQuery().replace(";", "")
+            + siteClause(includeQuery, coralSession) + ";";
+
+        final ResourceIdentifierResolver resolver = getResolver(coralSession);
+
+        LongSet included = coralSession.getRelationQuery().queryIds(catQuery, resolver, ids);
+
+        if(excludeQuery != null)
+        {
+            LongSet excluded = coralSession.getRelationQuery().queryIds(excludeQuery.getQuery(),
+                resolver, included);
+            included.removeAll(excluded);
+        }
+
+        List<DocumentNodeResource> documentList = new ArrayList<>(included.size());
+        LongIterator i = included.iterator();
+        while(i.hasNext())
+        {
+            documentList.add((DocumentNodeResource)coralSession.getStore().getResource(i.next()));
+        }
+
+        return new ResourceListTableModel<DocumentNodeResource>(documentList, i18nContext.get());
+    }
+
+    private String siteClause(CategoryQueryResource query, CoralSession coralSession)
+        throws SiteException
+    {
+        StringBuilder buff = new StringBuilder();
+        if(query.isAcceptedSitesDefined())
+        {
+            buff.append(" * MAP('structure.SiteDocs') { ");
+            String[] siteNames = query.getAcceptedSiteNames();
+            for(int i = 0; i < siteNames.length; i++)
+            {
+                SiteResource site = siteService.getSite(coralSession, siteNames[i]);
+                buff.append("RES(").append(site.getIdString()).append(")");
+                if(i < siteNames.length - 1)
+                {
+                    buff.append(" + ");
+                }
+            }
+            buff.append(" }");
+        }
+        return buff.toString();
+    }
+
+    private ResourceIdentifierResolver getResolver(final CoralSession coralSession)
+    {
+        return new ResourceIdentifierResolver()
+            {
+                @Override
+                public LongSet resolveIdentifier(String identifier)
+                    throws EntityDoesNotExistException
+                {
+                    LongSet ids;
+                    Resource res = null;
+                    if(identifier.startsWith("/"))
+                    {
+                        Resource[] ra = coralSession.getStore().getResourceByPath(identifier);
+                        if(ra.length == 0)
+                        {
+                            throw new EntityDoesNotExistException("resource " + identifier
+                                + " does not exist");
+                        }
+                        if(ra.length > 0)
+                        {
+                            throw new EntityDoesNotExistException("path " + identifier
+                                + " is ambiguous");
+                        }
+                        res = ra[0];
+                    }
+                    else if(identifier.matches("\\d+"))
+                    {
+                        res = coralSession.getStore().getResource(Long.parseLong(identifier));
+                    }
+                    else
+                    {
+                        throw new EntityDoesNotExistException("malformed identifier " + identifier);
+                    }
+                    if(res instanceof SiteResource)
+                    {
+                        ids = new LongOpenHashSet(1);
+                        ids.add(res.getId());
+                    }
+                    else if(res instanceof CategoryResource)
+                    {
+                        CategoryResource[] categories = categoryService.getSubCategories(
+                            coralSession, (CategoryResource)res, true);
+                        ids = new LongOpenHashSet(categories.length);
+                        for(int i = 0; i < categories.length; i++)
+                        {
+                            ids.add(categories[i].getId());
+                        }
+                    }
+                    else
+                    {
+                        throw new EntityDoesNotExistException(identifier + " has unexpected type "
+                            + res.getResourceClass().getName());
+                    }
+                    return ids;
+                }
+            };
+    }
+
+    private CategoryQueryResource getQueryResource(String name, Parameters screenConfig,
+        CoralSession coralSession)
+    {
+        long id = screenConfig.getLong(name + "_query_id", -1l);
+        if(id != -1l)
+        {
+            try
+            {
+                return (CategoryQueryResource)coralSession.getStore().getResource(id);
+            }
+            catch(EntityDoesNotExistException | ClassCastException e)
+            {
+            }
+        }
+        return null;
     }
 
     /**
@@ -297,7 +479,6 @@ public class ProposeDocument
         Parameters parameters = RequestParameters.getRequestParameters(context);
         CoralSession coralSession = context.getAttribute(CoralSession.class);
         TemplatingContext templatingContext = TemplatingContext.getTemplatingContext(context);
-        SiteResource site = getSite();
         try
         {
             // refill parameters in case we are coming back failed validation
