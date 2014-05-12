c142ed3a34f1dd25c5373c2326648c7e4c30cfdc
Lukasz Urbanski <lukasz@caltha.pl>
2014-05-12 14:58:38 +0200

    BAZYNGO-1639: null pointer exception fix.

--- cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/documents/MyDocumentsStateComparator.java
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/views/documents/MyDocumentsStateComparator.java
@@ -16,17 +16,23 @@ import org.objectledge.table.comparator.Direction;
  * @author <a href="mailto:lukasz@caltha.pl">Łukasz Urbański</a>
  * @version $Id: StateComparator.java $
  */
-public class MyDocumentsStateComparator<T extends DocumentNodeResource>  extends StateComparator<T>
+public class MyDocumentsStateComparator<T extends DocumentNodeResource>
+    extends StateComparator<T>
 {
     private final DocumentStateTool documentStateTool;
-    
-    private final List<String> myDocumentAllowedStateName = Arrays
-                    .asList(DocumentStateTool.DOCUMENT_STATES);
 
-    public MyDocumentsStateComparator(CoralSession coralSession, Logger logger, List<String> stateSortOrder, Direction direction)
+    public MyDocumentsStateComparator(CoralSession coralSession, Logger logger,
+        List<String> stateSortOrder, Direction direction)
     {
         super(direction);
-        stateSortOrder.retainAll(myDocumentAllowedStateName);
+        if(stateSortOrder != null)
+        {
+            stateSortOrder.retainAll(Arrays.asList(DocumentStateTool.DOCUMENT_STATES));
+        }
+        else
+        {
+            stateSortOrder = Arrays.asList(DocumentStateTool.DOCUMENT_STATES);
+        }
         this.stateSortOrderList = stateSortOrder;
         this.documentStateTool = new DocumentStateTool(coralSession, logger);
     }
@@ -34,13 +40,14 @@ public class MyDocumentsStateComparator<T extends DocumentNodeResource>  extends
     public MyDocumentsStateComparator(CoralSession coralSession, Logger logger, Direction direction)
     {
         super(direction);
-        this.stateSortOrderList = myDocumentAllowedStateName;
+        this.stateSortOrderList = Arrays.asList(DocumentStateTool.DOCUMENT_STATES);
         this.documentStateTool = new DocumentStateTool(coralSession, logger);
     }
 
     public int compare(T r1, T r2)
     {
-        int dif = stateSortOrderList.indexOf(documentStateTool.getState(r1)) - stateSortOrderList.indexOf(documentStateTool.getState(r2));
+        int dif = stateSortOrderList.indexOf(documentStateTool.getState(r1))
+            - stateSortOrderList.indexOf(documentStateTool.getState(r2));
         return Direction.ASC == this.direction ? dif : -dif;
     }
 }
