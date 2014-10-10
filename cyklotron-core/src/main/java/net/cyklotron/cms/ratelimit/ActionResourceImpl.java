f5123385534e01d85cdbc5e8394ceb76da965463
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-10-10 13:26:37 +0200

    LEDGE-255: Coral entities for storing rate limiting configuration

---
+++ cyklotron-core/src/main/java/net/cyklotron/cms/ratelimit/ActionResourceImpl.java
@@ -0,0 +1,262 @@
+// 
+// Copyright (c) 2004, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+// 
+// * Redistributions of source code must retain the above copyright notice,  
+//       this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//       this list of conditions and the following disclaimer in the documentation  
+//       and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//       nor the names of its contributors may be used to endorse or promote products  
+//       derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+ 
+package net.cyklotron.cms.ratelimit;
+
+import org.objectledge.coral.BackendException;
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.schema.AttributeDefinition;
+import org.objectledge.coral.schema.ResourceClass;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.store.InvalidResourceNameException;
+import org.objectledge.coral.store.ModificationNotPermitedException;
+import org.objectledge.coral.store.Resource;
+import org.objectledge.coral.store.ValueRequiredException;
+
+import net.cyklotron.cms.CmsNodeResourceImpl;
+
+/**
+ * An implementation of <code>cms.ratelimit.action</code> Coral resource class.
+ *
+ * @author Coral Maven plugin
+ */
+public class ActionResourceImpl
+    extends CmsNodeResourceImpl
+    implements ActionResource
+{
+    // class variables /////////////////////////////////////////////////////////
+
+    /** Class variables initialization status. */
+	@SuppressWarnings("unused")
+    private static boolean definitionsInitialized;
+	
+    /** The AttributeDefinition object for the <code>paramsOverride</code> attribute. */
+	private static AttributeDefinition<String> paramsOverrideDef;
+
+    /** The AttributeDefinition object for the <code>viewOverride</code> attribute. */
+	private static AttributeDefinition<String> viewOverrideDef;
+
+    // initialization /////////////////////////////////////////////////////////
+
+    /**
+     * Creates a blank <code>cms.ratelimit.action</code> resource wrapper.
+     *
+     * <p>This constructor should be used by the handler class only. Use 
+     * <code>load()</code> and <code>create()</code> methods to create
+     * instances of the wrapper in your application code.</p>
+     *
+     */
+    public ActionResourceImpl()
+    {
+    }
+
+    // static methods ////////////////////////////////////////////////////////
+
+    /**
+     * Retrieves a <code>cms.ratelimit.action</code> resource instance from the store.
+     *
+     * <p>This is a simple wrapper of StoreService.getResource() method plus
+     * the typecast.</p>
+     *
+     * @param session the CoralSession
+     * @param id the id of the object to be retrieved
+     * @return a resource instance.
+     * @throws EntityDoesNotExistException if the resource with the given id does not exist.
+     */
+    public static ActionResource getActionResource(CoralSession session, long id)
+        throws EntityDoesNotExistException
+    {
+        Resource res = session.getStore().getResource(id);
+        if(!(res instanceof ActionResource))
+        {
+            throw new IllegalArgumentException("resource #"+id+" is "+
+                                               res.getResourceClass().getName()+
+                                               " not cms.ratelimit.action");
+        }
+        return (ActionResource)res;
+    }
+
+    /**
+     * Creates a new <code>cms.ratelimit.action</code> resource instance.
+     *
+     * @param session the CoralSession
+     * @param name the name of the new resource
+     * @param parent the parent resource.
+     * @return a new ActionResource instance.
+     * @throws InvalidResourceNameException if the name argument contains illegal characters.
+     */
+    public static ActionResource createActionResource(CoralSession session, String name,
+        Resource parent)
+        throws InvalidResourceNameException
+    {
+        try
+        {
+            ResourceClass<ActionResource> rc = session.getSchema().getResourceClass("cms.ratelimit.action", ActionResource.class);
+		    Resource res = session.getStore().createResource(name, parent, rc,
+                java.util.Collections.<AttributeDefinition<?>, Object> emptyMap());			
+            if(!(res instanceof ActionResource))
+            {
+                throw new BackendException("incosistent schema: created object is "+
+                                           res.getClass().getName());
+            }
+            return (ActionResource)res;
+        }
+        catch(EntityDoesNotExistException e)
+        {
+            throw new BackendException("incompatible schema change", e);
+        }
+        catch(ValueRequiredException e)
+        {
+            throw new BackendException("incompatible schema change", e);
+        }
+    }
+
+    // public interface //////////////////////////////////////////////////////
+ 
+    /**
+     * Returns the value of the <code>paramsOverride</code> attribute.
+     *
+     * @return the value of the <code>paramsOverride</code> attribute.
+     */
+    public String getParamsOverride()
+    {
+        return get(paramsOverrideDef);
+    }
+    
+    /**
+     * Returns the value of the <code>paramsOverride</code> attribute.
+     *
+     * @param defaultValue the value to return if the attribute is undefined.
+     * @return the value of the <code>paramsOverride</code> attribute.
+     */
+    public String getParamsOverride(String defaultValue)
+    {
+        return get(paramsOverrideDef, defaultValue);
+    }    
+
+    /**
+     * Sets the value of the <code>paramsOverride</code> attribute.
+     *
+     * @param value the value of the <code>paramsOverride</code> attribute,
+     *        or <code>null</code> to remove value.
+     */
+    public void setParamsOverride(String value)
+    {
+        try
+        {
+            if(value != null)
+            {
+                set(paramsOverrideDef, value);
+            }
+            else
+            {
+                unset(paramsOverrideDef);
+            }
+        }
+        catch(ModificationNotPermitedException e)
+        {
+            throw new BackendException("incompatible schema change",e);
+        }
+        catch(ValueRequiredException e)
+        {
+            throw new BackendException("incompatible schema change",e);
+        }
+    }
+   
+	/**
+	 * Checks if the value of the <code>paramsOverride</code> attribute is defined.
+	 *
+	 * @return <code>true</code> if the value of the <code>paramsOverride</code> attribute is defined.
+	 */
+    public boolean isParamsOverrideDefined()
+	{
+	    return isDefined(paramsOverrideDef);
+	}
+ 
+    /**
+     * Returns the value of the <code>viewOverride</code> attribute.
+     *
+     * @return the value of the <code>viewOverride</code> attribute.
+     */
+    public String getViewOverride()
+    {
+        return get(viewOverrideDef);
+    }
+    
+    /**
+     * Returns the value of the <code>viewOverride</code> attribute.
+     *
+     * @param defaultValue the value to return if the attribute is undefined.
+     * @return the value of the <code>viewOverride</code> attribute.
+     */
+    public String getViewOverride(String defaultValue)
+    {
+        return get(viewOverrideDef, defaultValue);
+    }    
+
+    /**
+     * Sets the value of the <code>viewOverride</code> attribute.
+     *
+     * @param value the value of the <code>viewOverride</code> attribute,
+     *        or <code>null</code> to remove value.
+     */
+    public void setViewOverride(String value)
+    {
+        try
+        {
+            if(value != null)
+            {
+                set(viewOverrideDef, value);
+            }
+            else
+            {
+                unset(viewOverrideDef);
+            }
+        }
+        catch(ModificationNotPermitedException e)
+        {
+            throw new BackendException("incompatible schema change",e);
+        }
+        catch(ValueRequiredException e)
+        {
+            throw new BackendException("incompatible schema change",e);
+        }
+    }
+   
+	/**
+	 * Checks if the value of the <code>viewOverride</code> attribute is defined.
+	 *
+	 * @return <code>true</code> if the value of the <code>viewOverride</code> attribute is defined.
+	 */
+    public boolean isViewOverrideDefined()
+	{
+	    return isDefined(viewOverrideDef);
+	}
+  
+    // @custom methods ///////////////////////////////////////////////////////
+}
