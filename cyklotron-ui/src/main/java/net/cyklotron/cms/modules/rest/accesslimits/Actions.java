2004021db168cd4e8573ee51716b813c5b495d04
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-11-04 15:37:27 +0100

    LEDGE-255: REST endpoints for rules & actions management

---
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/rest/accesslimits/Actions.java
@@ -0,0 +1,188 @@
+package net.cyklotron.cms.modules.rest.accesslimits;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import org.objectledge.coral.entity.AmbigousEntityNameException;
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.entity.EntityInUseException;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.session.CoralSessionFactory;
+import org.objectledge.coral.store.InvalidResourceNameException;
+import org.objectledge.coral.store.Resource;
+import org.objectledge.coral.web.rest.RequireCoralRole;
+import org.objectledge.utils.StackTrace;
+
+import net.cyklotron.cms.accesslimits.ActionResource;
+import net.cyklotron.cms.accesslimits.ActionResourceImpl;
+
+@Path("/accesslimits/actions")
+@RequireCoralRole("cms.administrator")
+public class Actions
+{
+    private static final String ACTIONS_ROOT = "/cms/accesslimits/actions";
+
+    private final CoralSession coralSession;
+
+    private final UriInfo uriInfo;
+
+    @Inject
+    public Actions(CoralSessionFactory coralSessionFactory, UriInfo uriInfo)
+    {
+        this.uriInfo = uriInfo;
+        this.coralSession = coralSessionFactory.getCurrentSession();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveActions()
+    {
+        Resource[] actions = coralSession.getStore().getResourceByPath(ACTIONS_ROOT + "/*");
+        return Response.ok(ActionDto.create(actions)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createAction(ActionDto action)
+    {
+        try
+        {
+            Resource parent = coralSession.getStore().getUniqueResourceByPath(ACTIONS_ROOT);
+            ActionResource res = ActionResourceImpl.createActionResource(coralSession,
+                action.getName(), parent);
+            res.setViewOverride(action.getViewOverride());
+            res.setParamsOverride(action.getParamsOverride());
+            res.update();
+            return Response.created(uriInfo.getRequestUri().resolve(action.getName())).build();
+        }
+        catch(InvalidResourceNameException | EntityDoesNotExistException
+                        | AmbigousEntityNameException e)
+        {
+            return Response.status(Status.INTERNAL_SERVER_ERROR)
+                .entity(new StackTrace(e).toString()).build();
+        }
+    }
+
+    @PUT
+    @Path("{name}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateAction(@PathParam("name") String name, ActionDto action)
+    {
+        try
+        {
+            ActionResource current = (ActionResource)coralSession.getStore()
+                .getUniqueResourceByPath(ACTIONS_ROOT + "/" + name);
+            current.setViewOverride(action.getViewOverride());
+            current.setParamsOverride(action.getParamsOverride());
+            current.update();
+            return Response.noContent().build();
+        }
+        catch(EntityDoesNotExistException e)
+        {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        catch(ClassCastException | AmbigousEntityNameException e)
+        {
+            return Response.status(Status.INTERNAL_SERVER_ERROR)
+                .entity(new StackTrace(e).toString()).build();
+        }
+    }
+
+    @DELETE
+    @Path("{name}")
+    public Response deleteAction(@PathParam("name") String name)
+    {
+        try
+        {
+            ActionResource current = (ActionResource)coralSession.getStore()
+                .getUniqueResourceByPath(ACTIONS_ROOT + "/" + name);
+            coralSession.getStore().deleteResource(current);
+            return Response.noContent().build();
+        }
+        catch(EntityDoesNotExistException e)
+        {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        catch(ClassCastException | AmbigousEntityNameException | IllegalArgumentException
+                        | EntityInUseException e)
+        {
+            return Response.status(Status.INTERNAL_SERVER_ERROR)
+                .entity(new StackTrace(e).toString()).build();
+        }
+    }
+
+    public static class ActionDto
+    {
+        private String name;
+
+        private String viewOverride;
+
+        private String paramsOverride;
+
+        public ActionDto()
+        {
+        }
+
+        public ActionDto(ActionResource action)
+        {
+            name = action.getName();
+            viewOverride = action.getViewOverride();
+            paramsOverride = action.getParamsOverride();
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public String getViewOverride()
+        {
+            return viewOverride;
+        }
+
+        public void setViewOverride(String viewOverride)
+        {
+            this.viewOverride = viewOverride;
+        }
+
+        public String getParamsOverride()
+        {
+            return paramsOverride;
+        }
+
+        public void setParamsOverride(String paramsOverride)
+        {
+            this.paramsOverride = paramsOverride;
+        }
+
+        public static Collection<ActionDto> create(Resource[] actions)
+        {
+            List<ActionDto> result = new ArrayList<ActionDto>(actions.length);
+            for(Resource action : actions)
+            {
+                result.add(new ActionDto((ActionResource)action));
+            }
+            return result;
+        }
+    }
+}
---
+++ cyklotron-ui/src/main/java/net/cyklotron/cms/modules/rest/accesslimits/Rules.java
@@ -0,0 +1,467 @@
+package net.cyklotron.cms.modules.rest.accesslimits;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import org.objectledge.coral.entity.AmbigousEntityNameException;
+import org.objectledge.coral.entity.EntityDoesNotExistException;
+import org.objectledge.coral.entity.EntityInUseException;
+import org.objectledge.coral.session.CoralSession;
+import org.objectledge.coral.session.CoralSessionFactory;
+import org.objectledge.coral.store.InvalidResourceNameException;
+import org.objectledge.coral.store.Resource;
+import org.objectledge.coral.store.ValueRequiredException;
+import org.objectledge.coral.web.rest.RequireCoralRole;
+import org.objectledge.utils.StackTrace;
+import org.objectledge.web.ratelimit.impl.RuleFactory;
+import org.objectledge.web.ratelimit.rules.ParseException;
+
+import net.cyklotron.cms.accesslimits.ProtectedItemResource;
+import net.cyklotron.cms.accesslimits.ProtectedItemResourceImpl;
+import net.cyklotron.cms.accesslimits.RuleResource;
+import net.cyklotron.cms.accesslimits.RuleResourceImpl;
+
+@Path("/accesslimits/rules")
+@RequireCoralRole("cms.administrator")
+public class Rules
+{
+    private static final String RULES_ROOT = "/cms/accesslimits/rules";
+
+    private final CoralSession coralSession;
+
+    private final UriInfo uriInfo;
+
+    @Inject
+    public Rules(CoralSessionFactory coralSessionFactory, UriInfo uriInfo)
+    {
+        this.uriInfo = uriInfo;
+        this.coralSession = coralSessionFactory.getCurrentSession();
+    }
+
+    @GET
+    @Path("/items")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveProtectedItems()
+    {
+        Resource[] actions = coralSession.getStore().getResourceByPath(RULES_ROOT + "/*");
+        return Response.ok(ProtectedItemDao.create(actions)).build();
+    }
+
+    @POST
+    @Path("/items")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createProctectedItem(ProtectedItemDao item)
+    {
+        try
+        {
+            validateProtectedItem(item);
+        }
+        catch(PatternSyntaxException | ParseException e)
+        {
+            return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+
+        try
+        {
+            Resource parent = coralSession.getStore().getUniqueResourceByPath(RULES_ROOT);
+            String name = Integer.toString(parent.getChildren().length + 1); // I'm feeling lucky
+            ProtectedItemResource itemResource = ProtectedItemResourceImpl
+                .createProtectedItemResource(coralSession, name, parent, item.getUrlPattern());
+            int n = 1;
+            for(RuleDao rule : item.getRules())
+            {
+                RuleResourceImpl.createRuleResource(coralSession, Integer.toString(n),
+                    itemResource, n, rule.getRuleDefinition());
+                n++;
+            }
+            return Response.created(uriInfo.getRequestUri().resolve(name)).build();
+        }
+        catch(EntityDoesNotExistException | AmbigousEntityNameException | ValueRequiredException
+                        | InvalidResourceNameException e)
+        {
+            return Response.status(Status.INTERNAL_SERVER_ERROR)
+                .entity(new StackTrace(e).toString()).build();
+        }
+    }
+
+    @GET
+    @Path("/items/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveProtectedItem(@PathParam("name") String name)
+    {
+        try
+        {
+            Resource res = coralSession.getStore().getUniqueResourceByPath(RULES_ROOT + "/" + name);
+            return Response.ok(new ProtectedItemDao((ProtectedItemResource)res)).build();
+        }
+        catch(EntityDoesNotExistException | AmbigousEntityNameException e)
+        {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+    }
+
+    @PUT
+    @Path("/items/{name}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateProtectedItem(@PathParam("name") String name, ProtectedItemDao item)
+    {
+        try
+        {
+            validateProtectedItem(item);
+        }
+        catch(PatternSyntaxException | ParseException e)
+        {
+            return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+
+        try
+        {
+            ProtectedItemResource res = (ProtectedItemResource)coralSession.getStore()
+                .getUniqueResourceByPath(RULES_ROOT + "/" + name);
+            try
+            {
+                res.setUrlPattern(item.getUrlPattern());
+                res.update();
+                Resource[] cur = res.getChildren();
+                if(item.getRules() != null && item.getRules().size() > 0)
+                {
+                    int p = 1;
+                    for(RuleDao rule : item.getRules())
+                    {
+                        rule.setPriority(p++);
+                    }
+                    int n = cur.length + 1;
+                    for(RuleDao rule : item.getRules())
+                    {
+                        RuleResource curRule = getRuleResource(cur, rule.getId());
+                        if(curRule == null)
+                        {
+                            RuleResourceImpl.createRuleResource(coralSession,
+                                Integer.toString(n++), res, rule.getPriority(),
+                                rule.getRuleDefinition());
+                        }
+                        else
+                        {
+                            curRule.setPriority(rule.getPriority());
+                            curRule.setRuleDefinition(rule.getRuleDefinition());
+                            curRule.update();
+                        }
+                    }
+                    for(Resource curRule : cur)
+                    {
+                        if(getRuleDao(item.getRules(), curRule.getId()) == null)
+                        {
+                            coralSession.getStore().deleteResource(curRule);
+                        }
+                    }
+                }
+                else
+                {
+                    // no rules array in item - delete all rules from db
+                    for(Resource curRule : cur)
+                    {
+                        coralSession.getStore().deleteResource(curRule);
+                    }
+                }
+                return Response.noContent().build();
+            }
+            catch(Exception e)
+            {
+                return Response.status(Status.INTERNAL_SERVER_ERROR)
+                    .entity(new StackTrace(e).toString()).build();
+            }
+        }
+        catch(EntityDoesNotExistException | AmbigousEntityNameException | ClassCastException e)
+        {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+    }
+
+    private RuleResource getRuleResource(Resource[] rs, long id)
+    {
+        for(Resource r : rs)
+        {
+            if(r.getId() == id)
+            {
+                return (RuleResource)r;
+            }
+        }
+        return null;
+    }
+
+    private RuleDao getRuleDao(List<RuleDao> rules, long id)
+    {
+        for(RuleDao rule : rules)
+        {
+            if(rule.getId() != null && rule.getId() == id)
+            {
+                return rule;
+            }
+        }
+        return null;
+    }
+
+    @DELETE
+    @Path("/items/{name}")
+    public Response deleteProtectedItem(@PathParam("name") String name)
+    {
+        try
+        {
+            Resource res = coralSession.getStore().getUniqueResourceByPath(RULES_ROOT + "/" + name);
+            try
+            {
+                coralSession.getStore().deleteTree(res);
+                return Response.noContent().build();
+            }
+            catch(EntityInUseException e)
+            {
+                return Response.status(Status.INTERNAL_SERVER_ERROR)
+                    .entity(new StackTrace(e).toString()).build();
+            }
+        }
+        catch(EntityDoesNotExistException | AmbigousEntityNameException e)
+        {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+    }
+
+    @POST
+    @Path("/validate/urlPattern")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response validateUrlPattern(ValidationRequest request)
+    {
+        try
+        {
+            validateUrlPattern(request.getText());
+            return Response.ok(new ValidationResponse(true)).build();
+        }
+        catch(PatternSyntaxException e)
+        {
+            return Response.ok(new ValidationResponse(false, e.getMessage())).build();
+        }
+    }
+
+    @POST
+    @Path("/validate/rule")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response validateRuleDefinition(ValidationRequest request)
+    {
+        try
+        {
+            validateRuleDefinition(request.getText());
+            return Response.ok(new ValidationResponse(true)).build();
+        }
+        catch(ParseException e)
+        {
+            return Response.ok(new ValidationResponse(false, e.getMessage())).build();
+        }
+    }
+
+    private void validateUrlPattern(String urlPattern)
+        throws PatternSyntaxException
+    {
+        Pattern.compile(urlPattern);
+    }
+
+    private void validateRuleDefinition(String rule)
+        throws ParseException
+    {
+        RuleFactory.getInstance().validateRule(rule);
+    }
+
+    private void validateProtectedItem(ProtectedItemDao item)
+        throws PatternSyntaxException, ParseException
+    {
+        validateUrlPattern(item.getUrlPattern());
+        if(item.getRules() != null)
+        {
+            for(RuleDao rule : item.getRules())
+            {
+                validateRuleDefinition(rule.getRuleDefinition());
+            }
+        }
+    }
+
+    public static class ValidationRequest
+    {
+        private String text;
+
+        public String getText()
+        {
+            return text;
+        }
+
+        public void setText(String text)
+        {
+            this.text = text;
+        }
+    }
+
+    public static class ValidationResponse
+    {
+        private boolean valid;
+
+        private String error;
+
+        public ValidationResponse(boolean valid)
+        {
+            this.valid = valid;
+        }
+
+        public ValidationResponse(boolean valid, String error)
+        {
+            this.valid = valid;
+            this.error = error;
+        }
+
+        public boolean isValid()
+        {
+            return valid;
+        }
+
+        public void setValid(boolean valid)
+        {
+            this.valid = valid;
+        }
+
+        public String getError()
+        {
+            return error;
+        }
+
+        public void setError(String error)
+        {
+            this.error = error;
+        }
+    }
+
+    public static class ProtectedItemDao
+    {
+        private long id;
+
+        private String urlPattern;
+
+        private List<RuleDao> rules;
+
+        public ProtectedItemDao(ProtectedItemResource resource)
+        {
+            this.id = resource.getId();
+            this.urlPattern = resource.getUrlPattern();
+            this.rules = RuleDao.create(resource.getChildren());
+        }
+
+        public long getId()
+        {
+            return id;
+        }
+
+        public void setId(long id)
+        {
+            this.id = id;
+        }
+
+        public String getUrlPattern()
+        {
+            return urlPattern;
+        }
+
+        public void setUrlPattern(String urlPattern)
+        {
+            this.urlPattern = urlPattern;
+        }
+
+        public List<RuleDao> getRules()
+        {
+            return rules;
+        }
+
+        public void setRules(List<RuleDao> rules)
+        {
+            this.rules = rules;
+        }
+
+        public static List<ProtectedItemDao> create(Resource[] items)
+        {
+            List<ProtectedItemDao> result = new ArrayList<ProtectedItemDao>(items.length);
+            for(Resource item : items)
+            {
+                result.add(new ProtectedItemDao((ProtectedItemResource)item));
+            }
+            return result;
+        }
+    }
+
+    public static class RuleDao
+    {
+        private Long id;
+
+        private int priority;
+
+        private String ruleDefinition;
+
+        public RuleDao(RuleResource resource)
+        {
+            this.id = resource.getId();
+            this.priority = resource.getPriority();
+            this.ruleDefinition = resource.getRuleDefinition();
+        }
+
+        public Long getId()
+        {
+            return id;
+        }
+
+        public void setId(Long id)
+        {
+            this.id = id;
+        }
+
+        public int getPriority()
+        {
+            return priority;
+        }
+
+        public void setPriority(int priority)
+        {
+            this.priority = priority;
+        }
+
+        public String getRuleDefinition()
+        {
+            return ruleDefinition;
+        }
+
+        public void setRuleDefinition(String ruleDefinition)
+        {
+            this.ruleDefinition = ruleDefinition;
+        }
+
+        public static List<RuleDao> create(Resource[] rules)
+        {
+            List<RuleDao> result = new ArrayList<RuleDao>(rules.length);
+            for(Resource rule : rules)
+            {
+                result.add(new RuleDao((RuleResource)rule));
+            }
+            return result;
+        }
+    }
+}
